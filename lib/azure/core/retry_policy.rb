beb5af33f090f328903d3d3ed620b76892e56ec2
Troy Howard <troy@appfog.com>
2012-08-31 16:23:43 -0700

    Add HttpFilter and RetryPolicy to Communication layer (closes #33)

---
+++ lib/azure/core/retry_policy.rb
@@ -0,0 +1,59 @@
+#-------------------------------------------------------------------------
+# Copyright 2012 Microsoft Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+require('/azure/core/http_filter')
+
+module Azure
+  module Core
+    
+    # A HttpFilter implementation that handles retrying based on a 
+    # specific policy when HTTP layer errors occur
+    class RetryPolicy < HttpFilter
+  
+    def initialize(&block)
+      @block = block
+    end
+
+    attr_accessor :retry_data
+
+    # Overrides the base class implementation of call to implement 
+    # a retry loop that uses should_retry? to determine when to 
+    # break the loop
+    def call(req, _next)
+      retry_data = {}
+      response = nil
+      begin
+        response = _next.call
+      end while should_retry?(response, retry_data)
+      response
+    end
+
+    # Determines if the HTTP request should continue retrying
+    # 
+    # If an inline block was passed to the constructor, that block 
+    # will be used here and should return true to retry the job, or
+    # false to stop exit.
+    # 
+    # The retry_data parameter is a Hash which can be used to store 
+    # stateful data about the request execution context (such as an 
+    # incrementing counter, timestamp, etc). The retry_data object 
+    # will be the same instance throughout the lifetime of the request.
+    #
+    # If an inline block was not passed to the constructor 
+    # the method returns false
+    def should_try?(response, retry_data)
+      @block ? @block.call(response, retry_data) : false
+    end
+  end
+end
