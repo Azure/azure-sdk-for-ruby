20e922e552cfd6d504b62155bdc74a5c7456055d
Troy Howard <troy@appfog.com>
2012-09-27 15:54:34 -0700

    add uri methods and default timeout support

--- lib/azure/service/blob_service.rb
+++ lib/azure/service/blob_service.rb
@@ -17,8 +17,74 @@ require 'azure/service/storage_service'
 module Azure
   module Service
     class BlobService < StorageService
+
       def initialize
         super()
+        @default_timeout = 90
+      end
+
+      attr_accessor :default_timeout
+
+      # Public: Generate the URI for the collection of containers.
+      #
+      # query - A Hash of key => value query parameters.
+      # host  - The host of the API.
+      #
+      # Returns a URI.
+      def containers_uri(query={}, host=Azure.config.blob_host)
+        query = { "comp" => "list" }.merge(query)
+        uri = join(host, "/")
+        add_query(uri, query)
+      end
+
+      # Public: Generate the URI for a specific container.
+      #
+      # name  - The container name. If this is a URI, we just return this.
+      # query - A Hash of key => value query parameters.
+      # host  - The host of the API.
+      #
+      # Returns a URI.
+      def container_uri(name, query={}, host=Azure.config.blob_host)
+        return name if name.kind_of? ::URI
+        query = { "restype" => "container" }.merge(query)
+        uri = join(host, name)
+        add_query(uri, query)
+      end
+
+      # Public: Generate the URI for a specific Blob.
+      #
+      # container_name - String representing the name of the container.
+      # blob_name      - String representing the name of the blob.
+      # query          - A Hash of key => value query parameters.
+      # host           - The host of the API.
+      #
+      # Returns a URI.
+      def blob_uri(container_name, blob_name, query={}, host=Azure.config.blob_host)
+        uri = join(host, File.join(container_name, blob_name))
+        add_query(uri, query)
+      end
+
+      # Public: Adds query to URI, setting default timeout unless it's already in query
+      #
+      # uri   - The URI.
+      # query - A Hash of key => value query paramters
+      #
+      # Returns as URI.
+      def add_query(uri, query={})
+        query["timeout"] = default_timeout.to_s unless query.has_key? "timeout"
+
+        uri.query = ::URI.encode_www_form(query)
+        uri
+      end
+
+      # Utility method to generate the URI.
+      #
+      # host - A String with the URI's host.
+      # path - A String with the URI's path.
+      #
+      # Returns a URI.
+      def join(host, path)
+        ::URI.parse(File.join(host, path))
       end
     end
   end
