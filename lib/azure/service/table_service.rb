a4af0c2ac6a0c19f131e7495849049bd66f08c74
Troy Howard <troy@appfog.com>
2012-10-01 16:04:22 -0700

    add uri generation methods to TableService

--- lib/azure/service/table_service.rb
+++ lib/azure/service/table_service.rb
@@ -22,6 +22,46 @@ module Azure
         @host = Azure.config.table_host
         @default_timeout = 30
       end
+
+      # Public: Generate the URI for the collection of tables.
+      #
+      # Returns a URI
+      def collection_uri()
+        generate_uri("Tables")
+      end
+
+      # Public: Generate the URI for a specific table.
+      #
+      # name - The table name. If this is a URI, we just return this
+      #
+      # Returns a URI
+      def table_uri(name)
+        return name if name.kind_of? ::URI
+        generate_uri("Tables('#{name}')")
+      end
+
+      # Public: Generate the URI for an entity or group of entities in a table.
+      # If both the 'partition_key' and 'row_key' are specified, then the URI
+      # will match the entity under those specific keys.
+      #
+      # table_name    - The table name
+      # partition_key - The desired partition key (optional)
+      # row_key       - The desired row key (optional)
+      #
+      # Returns a URI
+      def entities_uri(table_name, partition_key=nil, row_key=nil)
+        return table_name if table_name.kind_of? ::URI
+
+        path = if partition_key && row_key
+          "%s(PartitionKey='%s',RowKey='%s')" % [
+            table_name, partition_key, row_key
+          ]
+        else
+          "%s()" % table_name
+        end
+
+        generate_uri(path)
+      end
     end
   end
 end
