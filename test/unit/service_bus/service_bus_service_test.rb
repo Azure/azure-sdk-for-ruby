dc2640a84e40b7bfd8d02689d9f2e9f44f853150
Troy Howard <troy@appfog.com>
2012-10-11 23:16:42 -0700

    unit test stubs for ServiceBusService and WrapService

--- test/unit/service_bus/service_bus_service_test.rb
+++ test/unit/service_bus/service_bus_service_test.rb
@@ -17,4 +17,145 @@ require 'azure/service_bus/service_bus_service'
 
 describe Azure::ServiceBus::ServiceBusService do
   subject { Azure::ServiceBus::ServiceBusService.new }
+  
+  describe '#create_queue' do
+    it '' do
+      
+    end
+  end
+  describe '#delete_queue' do
+    it '' do
+      
+    end
+  end
+  describe '#get_queue' do
+    it '' do
+      
+    end
+  end
+  describe '#list_queues' do
+    it '' do
+      
+    end
+  end
+  describe '#create_topic' do
+    it '' do
+      
+    end
+  end
+  describe '#delete_topic' do
+    it '' do
+      
+    end
+  end
+  describe '#get_topic' do
+    it '' do
+      
+    end
+  end
+  describe '#list_topics' do
+    it '' do
+      
+    end
+  end
+  describe '#create_rule' do
+    it '' do
+      
+    end
+  end
+  describe '#delete_rule' do
+    it '' do
+      
+    end
+  end
+  describe '#get_rule' do
+    it '' do
+      
+    end
+  end
+  describe '#list_rules' do
+    it '' do
+      
+    end
+  end
+  describe '#create_subscription' do
+    it '' do
+      
+    end
+  end
+  describe '#delete_subscription' do
+    it '' do
+      
+    end
+  end
+  describe '#get_subscription' do
+    it '' do
+      
+    end
+  end
+  describe '#list_subscriptions' do
+    it '' do
+      
+    end
+  end
+  describe '#send_topic_message' do
+    it '' do
+      
+    end
+  end
+  describe '#peek_lock_subscription_message' do
+    it '' do
+      
+    end
+  end
+  describe '#unlock_subscription_message' do
+    it '' do
+      
+    end
+  end
+  describe '#read_delete_subscription_message' do
+    it '' do
+      
+    end
+  end
+  describe '#delete_subscription_message' do
+    it '' do
+      
+    end
+  end
+  describe '#send_queue_message' do
+    it '' do
+      
+    end
+  end
+  describe '#peek_lock_queue_message' do
+    it '' do
+      
+    end
+  end
+  describe '#unlock_queue_message' do
+    it '' do
+      
+    end
+  end
+  describe '#read_delete_queue_message' do
+    it '' do
+      
+    end
+  end
+  describe '#delete_queue_message' do
+    it '' do
+      
+    end
+  end
+  describe '#receive_queue_message' do
+    it '' do
+      
+    end
+  end
+  describe '#receive_subscription_message' do
+    it '' do
+      
+    end
+  end
 end
