39a09dda29ac6d7529df85a2734f99a3b2d45dd2
Troy Howard <troy@appfog.com>
2012-10-17 20:59:38 -0700

    update version to latest

--- lib/azure/core/http/http_request.rb
+++ lib/azure/core/http/http_request.rb
@@ -93,7 +93,7 @@ module Azure
         # Build a default headers Hash
         def default_headers(current_time)
           headers["x-ms-date"] = current_time
-          headers["x-ms-version"] = "2011-08-18"
+          headers["x-ms-version"] = "2012-02-12"
           headers["DataServiceVersion"] = "1.0;NetFx"
           headers["MaxDataServiceVersion"] = "2.0;NetFx"
 
