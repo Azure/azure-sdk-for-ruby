ee424a2cd0bf582d988e172fe2f1851ac4eef15b
Troy Howard <thoward37@gmail.com>
2012-10-22 09:53:19 -0700

    integration tests for BlobService

--- test/integration/storage/blob/create_block_blob_test.rb
+++ test/integration/storage/blob/create_block_blob_test.rb
@@ -19,8 +19,44 @@ describe Azure::Storage::Blob::BlobService do
   subject { Azure::Storage::Blob::BlobService.new }
   
   describe '#create_block_blob' do
-    it '' do
-      
+    let(:container_name) { ContainerNameHelper.name }
+    let(:blob_name) { "blobname" }
+    let(:content) { content = ""; 512.times.each{|i| content << "@" }; content }
+    before { 
+      subject.create_container container_name
+    }
+    after { TableNameHelper.clean }
+
+    it 'creates a page blob' do
+      blob = subject.create_block_blob container_name, blob_name, content
+      blob.name.must_equal blob_name
+    end
+
+    it 'sets additional properties when the options hash is used' do
+      options = {
+        :content_type=>"application/xml",
+        :content_encoding=>"utf-8",
+        :content_language=>"en-US",
+        :cache_control=>"max-age=1296000",
+        :metadata => { "CustomMetadataProperty"=>"CustomMetadataValue"}
+      }
+
+      blob = subject.create_block_blob container_name, blob_name, content, options
+      blob = subject.get_blob_properties container_name, blob_name
+      blob.name.must_equal blob_name
+      blob.properties.content_type.must_equal options[:content_type]
+      blob.properties.content_encoding.must_equal options[:content_encoding]
+      blob.properties.cache_control.must_equal options[:cache_control]
+
+      blob = subject.get_blob_metadata container_name, blob_name
+      blob.name.must_equal blob_name
+      blob.metadata["custommetadataproperty"].must_equal "CustomMetadataValue"
+    end
+
+    it 'errors if the container does not exist' do
+      assert_raises(Azure::Core::Http::HTTPError) do
+        subject.create_block_blob ContainerNameHelper.name, blob_name, content
+      end
     end
   end
 end
