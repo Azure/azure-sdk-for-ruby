8ed2b5ac302bb48ec9c8fe634440878cc25d40e7
Andre Rodrigues <andrerod@microsoft.com>
2012-12-19 16:54:22 +0000

    106: Inconsistent use of optional parameters vs options parameters

--- test/integration/service_bus/subscriptions_test.rb
+++ test/integration/service_bus/subscriptions_test.rb
@@ -15,12 +15,12 @@
 require "integration/test_helper"
 
 describe "ServiceBus Subscriptions" do
-	subject { Azure::ServiceBus::ServiceBus.new }
-	after { ServiceBusTopicNameHelper.clean }
-	let(:topic){ ServiceBusTopicNameHelper.name }
-	let(:subscription) { "mySubscription" }
+  subject { Azure::ServiceBus::ServiceBus.new }
+  after { ServiceBusTopicNameHelper.clean }
+  let(:topic){ ServiceBusTopicNameHelper.name }
+  let(:subscription) { "mySubscription" }
 
-	before { subject.create_topic topic }
+  before { subject.create_topic topic }
 
   it "should be able to create a new subscription" do
     result = subject.create_subscription topic, subscription
@@ -29,106 +29,106 @@ describe "ServiceBus Subscriptions" do
   end
 
   describe "when a subscription exists" do
-  	before { subject.create_subscription topic, subscription }
-	  it "should be able to delete the subscription" do
-	    subject.delete_subscription topic, subscription
-	  end
+    before { subject.create_subscription topic, subscription }
+    it "should be able to delete the subscription" do
+      subject.delete_subscription topic, subscription
+    end
 
-	  it "should be able to get the subscription" do
-		  result = subject.get_subscription topic, subscription
-	    result.must_be :kind_of?, Azure::ServiceBus::Subscription
-	    result.name.must_equal subscription
-	  end
+    it "should be able to get the subscription" do
+      result = subject.get_subscription topic, subscription
+      result.must_be :kind_of?, Azure::ServiceBus::Subscription
+      result.name.must_equal subscription
+    end
 
-	  it "should be able to list subscriptions" do
-	  	result = subject.list_subscriptions topic
-	  	subscription_found = false
-	  	result.each { |s|
-	  		subscription_found = true if s.name == subscription
-	  	}
-	  	assert subscription_found, "list_subscriptions didn't include the expected subscription"
-	  end
+    it "should be able to list subscriptions" do
+      result = subject.list_subscriptions topic
+      subscription_found = false
+      result.each { |s|
+        subscription_found = true if s.name == subscription
+      }
+      assert subscription_found, "list_subscriptions didn't include the expected subscription"
+    end
 
-	  describe "when there are messages" do
-	  	let(:msg) { 
-	  		m = Azure::ServiceBus::BrokeredMessage.new("some message body", {:prop1 => "val1"})
-	  		m.to = "me"
-	  		m.label = "my_label"
-	  		m
-	  	}
-	  	before { 
-		  	subject.send_topic_message topic, msg
-	  	}
+    describe "when there are messages" do
+      let(:msg) { 
+        m = Azure::ServiceBus::BrokeredMessage.new("some message body", {:prop1 => "val1"})
+        m.to = "me"
+        m.label = "my_label"
+        m
+      }
+      before { 
+        subject.send_topic_message topic, msg
+      }
 
-		  it "should be able to peek lock a message" do
-		  	retrieved = subject.peek_lock_subscription_message topic, subscription
+      it "should be able to peek lock a message" do
+        retrieved = subject.peek_lock_subscription_message topic, subscription
 
-		  	retrieved.to.must_equal msg.to
-		    retrieved.body.must_equal msg.body
-		  	retrieved.label.must_equal msg.label
+        retrieved.to.must_equal msg.to
+        retrieved.body.must_equal msg.body
+        retrieved.label.must_equal msg.label
 
-		    retrieved = subject.read_delete_subscription_message topic, subscription, 1
-		    retrieved.must_be_nil
-		  end
-		  
-		  it "should be able to read delete a message" do
-		    retrieved = subject.read_delete_subscription_message topic, subscription
+        retrieved = subject.read_delete_subscription_message topic, subscription, 1
+        retrieved.must_be_nil
+      end
+      
+      it "should be able to read delete a message" do
+        retrieved = subject.read_delete_subscription_message topic, subscription
 
-		    retrieved.must_be :kind_of?, Azure::ServiceBus::BrokeredMessage
-		    retrieved.body.must_equal msg.body
-		    retrieved.to.must_equal msg.to
+        retrieved.must_be :kind_of?, Azure::ServiceBus::BrokeredMessage
+        retrieved.body.must_equal msg.body
+        retrieved.to.must_equal msg.to
 
-		    # it should be deleted
-		    retrieved = subject.read_delete_subscription_message topic, subscription, 1
-		    retrieved.must_be_nil
-		  end
+        # it should be deleted
+        retrieved = subject.read_delete_subscription_message topic, subscription, 1
+        retrieved.must_be_nil
+      end
 
-		  it "should be able to unlock a message" do
-		    retrieved = subject.peek_lock_subscription_message topic, subscription, 1
-		    retrieved.body.must_equal msg.body
+      it "should be able to unlock a message" do
+        retrieved = subject.peek_lock_subscription_message topic, subscription, 1
+        retrieved.body.must_equal msg.body
 
-		    # There shouldn't be an available message in the queue
-		    retrieved2 = subject.peek_lock_subscription_message topic, subscription, 1
-		    retrieved2.must_be_nil
+        # There shouldn't be an available message in the queue
+        retrieved2 = subject.peek_lock_subscription_message topic, subscription, 1
+        retrieved2.must_be_nil
 
-		    # Unlock the message
-		    res = subject.unlock_subscription_message topic, subscription, retrieved.sequence_number, retrieved.lock_token
-		    res.must_be_nil
+        # Unlock the message
+        res = subject.unlock_subscription_message topic, subscription, retrieved.sequence_number, retrieved.lock_token
+        res.must_be_nil
 
-		    # The message should be available once again
-		    retrieved = subject.peek_lock_subscription_message topic, subscription, 1
-		    retrieved.body.must_equal msg.body
-		  end
-		end
+        # The message should be available once again
+        retrieved = subject.peek_lock_subscription_message topic, subscription, 1
+        retrieved.body.must_equal msg.body
+      end
+    end
 
-	  describe 'when there are multiple subscriptions' do
-	  	let(:subscription1) { ServiceBusTopicNameHelper.name }
-	  	let(:subscription2) { ServiceBusTopicNameHelper.name }
+    describe 'when there are multiple subscriptions' do
+      let(:subscription1) { ServiceBusTopicNameHelper.name }
+      let(:subscription2) { ServiceBusTopicNameHelper.name }
 
-	  	before { 
-	  		subject.create_subscription topic, subscription1
-	  		subject.create_subscription topic, subscription2
-	  	}
+      before { 
+        subject.create_subscription topic, subscription1
+        subject.create_subscription topic, subscription2
+      }
 
-		  it "should be able to list subscriptions" do
-		  	result = subject.list_subscriptions topic
+      it "should be able to list subscriptions" do
+        result = subject.list_subscriptions topic
 
-		  	subscription_found = false
-		  	subscription1_found = false
-		  	subscription2_found = false
-		  	
-		  	result.each { |s|
-		  		subscription_found = true if s.name == subscription
-		  		subscription1_found = true if s.name == subscription1
-		  		subscription2_found = true if s.name == subscription2
-		  	}
-		  	
-		  	assert (subscription_found and subscription1_found and subscription2_found), "list_subscriptions didn't include the expected subscriptions"
-		  end
+        subscription_found = false
+        subscription1_found = false
+        subscription2_found = false
+
+        result.each { |s|
+          subscription_found = true if s.name == subscription
+          subscription1_found = true if s.name == subscription1
+          subscription2_found = true if s.name == subscription2
+        }
+
+        assert (subscription_found and subscription1_found and subscription2_found), "list_subscriptions didn't include the expected subscriptions"
+      end
 
       it "should be able to use $skip token" do
         result = subject.list_subscriptions topic
-        result2 = subject.list_subscriptions topic, 1
+        result2 = subject.list_subscriptions topic, { :skip => 1 }
         result2.length.must_equal result.length - 1
         result2[0].id.must_equal result[1].id
       end
@@ -137,16 +137,16 @@ describe "ServiceBus Subscriptions" do
         result = subject.list_subscriptions topic
         result.length.wont_equal 1
 
-        result2 = subject.list_subscriptions topic, nil, 1
+        result2 = subject.list_subscriptions topic, { :top => 1 }
         result2.length.must_equal 1
       end
 
       it "should be able to use $skip and $top token together" do
         result = subject.list_subscriptions topic
-        result2 = subject.list_subscriptions topic, 1, 1
+        result2 = subject.list_subscriptions topic, { :skip => 1, :top => 1 }
         result2.length.must_equal 1
         result2[0].id.must_equal result[1].id
       end
-		end
-	end
+    end
+  end
 end
--- test/integration/service_bus/topics_test.rb
+++ test/integration/service_bus/topics_test.rb
@@ -15,9 +15,9 @@
 require "integration/test_helper"
 
 describe "ServiceBus Topics" do
-	subject { Azure::ServiceBus::ServiceBus.new }
-	after { ServiceBusTopicNameHelper.clean }
-	let(:topic){ ServiceBusTopicNameHelper.name }
+  subject { Azure::ServiceBus::ServiceBus.new }
+  after { ServiceBusTopicNameHelper.clean }
+  let(:topic){ ServiceBusTopicNameHelper.name }
 
   it "should be able to create a new topic" do
     result = subject.create_topic topic
@@ -26,61 +26,61 @@ describe "ServiceBus Topics" do
   end
 
   describe "when a topic exists" do
-  	before { subject.create_topic topic }
+    before { subject.create_topic topic }
 
-	  it "should be able to delete the topic" do
-	    subject.delete_topic topic
-	  end
+    it "should be able to delete the topic" do
+      subject.delete_topic topic
+    end
 
-	  it "should be able to get the topic" do
-		  result = subject.get_topic topic
-	    result.must_be :kind_of?, Azure::ServiceBus::Topic
-	    result.name.must_equal topic
-	  end
+    it "should be able to get the topic" do
+      result = subject.get_topic topic
+      result.must_be :kind_of?, Azure::ServiceBus::Topic
+      result.name.must_equal topic
+    end
 
-	  it "should be able to list topics" do
-	  	result = subject.list_topics
-	  	topic_found = false
-	  	result.each { |t|
-	  		topic_found = true if t.name == topic
-	  	}
-	  	assert topic_found, "list_topics didn't include the expected topic"
-	  end
+    it "should be able to list topics" do
+      result = subject.list_topics
+      topic_found = false
+      result.each { |t|
+        topic_found = true if t.name == topic
+      }
+      assert topic_found, "list_topics didn't include the expected topic"
+    end
 
-	  it "should be able to send the topic a message" do
-	    message = Azure::ServiceBus::BrokeredMessage.new("some text", {:prop1 => "val1"}) do |m|
-	      m.to = "me"
-	      m.label = 'my_label'
-	    end
-	  	result = subject.send_topic_message topic, message
-	  	result.must_be_nil
-	  end
+    it "should be able to send the topic a message" do
+      message = Azure::ServiceBus::BrokeredMessage.new("some text", {:prop1 => "val1"}) do |m|
+        m.to = "me"
+        m.label = 'my_label'
+      end
+      result = subject.send_topic_message topic, message
+      result.must_be_nil
+    end
 
-	  describe 'when there are multiple topics' do
-	  	let(:topic1) { ServiceBusTopicNameHelper.name }
-	  	let(:topic2) { ServiceBusTopicNameHelper.name }
+    describe 'when there are multiple topics' do
+      let(:topic1) { ServiceBusTopicNameHelper.name }
+      let(:topic2) { ServiceBusTopicNameHelper.name }
 
-	  	before { 
-	  		subject.create_topic topic1
-	  		subject.create_topic topic2
-	  	}
+      before { 
+        subject.create_topic topic1
+        subject.create_topic topic2
+      }
 
-		  it "should be able to list topics" do
-		  	result = subject.list_topics
-		  	topic_found = false
-		  	topic1_found = false
-		  	topic2_found = false
-		  	result.each { |t|
-		  		topic_found = true if t.name == topic
-		  		topic1_found = true if t.name == topic1
-		  		topic2_found = true if t.name == topic2
-		  	}
-		  	assert (topic_found and topic1_found and topic2_found), "list_topics didn't include the expected topic"
-		  end
+      it "should be able to list topics" do
+        result = subject.list_topics
+        topic_found = false
+        topic1_found = false
+        topic2_found = false
+        result.each { |t|
+          topic_found = true if t.name == topic
+          topic1_found = true if t.name == topic1
+          topic2_found = true if t.name == topic2
+        }
+        assert (topic_found and topic1_found and topic2_found), "list_topics didn't include the expected topic"
+      end
 
       it "should be able to use $skip token with list_topics" do
         result = subject.list_topics
-        result2 = subject.list_topics 1
+        result2 = subject.list_topics({ :skip => 1 })
         result2.length.must_equal result.length - 1
         result2[0].id.must_equal result[1].id
       end
@@ -89,16 +89,16 @@ describe "ServiceBus Topics" do
         result = subject.list_topics
         result.length.wont_equal 1
 
-        result2 = subject.list_topics nil, 1
+        result2 = subject.list_topics({ :top => 1 })
         result2.length.must_equal 1
       end
 
       it "should be able to use $skip and $top token together with list_topics" do
         result = subject.list_topics
-        result2 = subject.list_topics 1, 1
+        result2 = subject.list_topics({ :skip => 1, :top => 1 })
         result2.length.must_equal 1
         result2[0].id.must_equal result[1].id
       end
-		end
-	end
+    end
+  end
 end
