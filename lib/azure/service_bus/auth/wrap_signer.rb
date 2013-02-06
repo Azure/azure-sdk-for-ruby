9e1d779fc3bde290a39d5936f6f03a87124d67bd
Andre Rodrigues <andrerod@microsoft.com>
2013-02-06 16:09:44 +0000

    199: Need to use less info in URL when requesting WRAP tokens

--- lib/azure/service_bus/auth/wrap_signer.rb
+++ lib/azure/service_bus/auth/wrap_signer.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #--------------------------------------------------------------------------
+require "uri"
+
 require "azure/core/auth/signer"
 require "azure/service_bus/auth/wrap_service"
 
@@ -20,32 +22,48 @@ module Azure
     module Auth
       class WrapSigner < Azure::Core::Auth::Signer
 
-      	def initialize
-      		@tokens = {}
-      		@wrap_service = Azure::ServiceBus::Auth::WrapService.new
-      	end
+        def initialize
+          @tokens = {}
+          @wrap_service = Azure::ServiceBus::Auth::WrapService.new
+        end
 
-      	attr_accessor :tokens
+        attr_accessor :tokens
 
         def name
           "WRAP"
         end
 
+        public
         def sign(method, uri, headers)
-          access_token = get_access_token(uri)
+          access_token = get_access_token(create_scope_uri(uri))
           'access_token="%s"' % access_token
         end
 
+        private
         def get_access_token(uri)
-        	token = tokens[uri.to_s]
-      		token = tokens[uri.to_s] = @wrap_service.get_access_token(uri) unless valid_token?(token)
-        	token[:token]
+          token = tokens[uri.to_s]
+          token = tokens[uri.to_s] = @wrap_service.get_access_token(uri) unless valid_token?(token)
+          token[:token]
         end
 
-        def valid_token?(token)
-        	token and token[:expiration] > Time.now.to_i
+        private
+        def create_scope_uri(target_uri)
+          targetUriComponents = URI.parse(target_uri.to_s)
+
+          # ACS body and caching should be HTTP
+          targetUriComponents.scheme = 'http'
+
+          # ACS body and caching should not include query
+          targetUriComponents.query = nil
+
+          targetUriComponents
         end
-    	end
+
+        private
+        def valid_token?(token)
+          token and token[:expiration] > Time.now.to_i
+        end
+      end
     end
   end
 end
