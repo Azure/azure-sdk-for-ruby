b82a8a0d635fc841eda1c398b7b06e4973a59c82
Andre Rodrigues <andrerod@microsoft.com>
2013-02-11 18:57:23 +0000

    replaced defined?

--- lib/azure/service_bus/queue.rb
+++ lib/azure/service_bus/queue.rb
@@ -41,17 +41,17 @@ module Azure
       #
       def initialize(name, options = {})
         normalized_options = {}
-        normalized_options["DefaultMessageTimeToLive"] = options[:default_message_time_to_live].to_s if defined? options[:default_message_time_to_live]
-        normalized_options["DuplicateDetectionHistoryTimeWindow"] = options[:duplicate_detection_history_time_window].to_s if defined? options[:duplicate_detection_history_time_window]
-        normalized_options["EnableBatchedOperations"] = options[:enable_batched_operations].to_s if defined? options[:enable_batched_operations]
-        normalized_options["DeadLetteringOnMessageExpiration"] = options[:dead_lettering_on_message_expiration].to_s if defined? options[:dead_lettering_on_message_expiration]
-        normalized_options["LockDuration"] = options[:lock_duration].to_s if defined? options[:lock_duration]
-        normalized_options["MaxDeliveryCount"] = options[:max_delivery_count].to_s if defined? options[:max_delivery_count]
-        normalized_options["MaxSizeInMegabytes"] = options[:max_size_in_megabytes].to_s if defined? options[:max_size_in_megabytes]
-        normalized_options["MessageCount"] = options[:message_count].to_s if defined? options[:message_count]
-        normalized_options["RequiresDuplicateDetection"] = options[:requires_duplicate_detection].to_s if defined? options[:requires_duplicate_detection]
-        normalized_options["RequiresSession"] = options[:requires_session].to_s if defined? options[:requires_session]
-        normalized_options["SizeInBytes"] = options[:size_in_bytes].to_s if defined? options[:size_in_bytes]
+        normalized_options["DefaultMessageTimeToLive"] = options[:default_message_time_to_live].to_s if options.has_key?(:default_message_time_to_live)
+        normalized_options["DuplicateDetectionHistoryTimeWindow"] = options[:duplicate_detection_history_time_window].to_s if options.has_key?(:duplicate_detection_history_time_window)
+        normalized_options["EnableBatchedOperations"] = options[:enable_batched_operations].to_s if options.has_key?(:enable_batched_operations)
+        normalized_options["DeadLetteringOnMessageExpiration"] = options[:dead_lettering_on_message_expiration].to_s if options.has_key?(:dead_lettering_on_message_expiration)
+        normalized_options["LockDuration"] = options[:lock_duration].to_s if options.has_key?(:lock_duration)
+        normalized_options["MaxDeliveryCount"] = options[:max_delivery_count].to_s if options.has_key?(:max_delivery_count)
+        normalized_options["MaxSizeInMegabytes"] = options[:max_size_in_megabytes].to_s if options.has_key?(:max_size_in_megabytes)
+        normalized_options["MessageCount"] = options[:message_count].to_s if options.has_key?(:message_count)
+        normalized_options["RequiresDuplicateDetection"] = options[:requires_duplicate_detection].to_s if options.has_key?(:requires_duplicate_detection)
+        normalized_options["RequiresSession"] = options[:requires_session].to_s if options.has_key?(:requires_session)
+        normalized_options["SizeInBytes"] = options[:size_in_bytes].to_s if options.has_key?(:size_in_bytes)
 
         super(name, normalized_options)
       end
