496cd4937379876eb32fb1da74607a7448e3db89
sandstrom <alexander@skovik.com>
2013-08-05 16:41:02 +0200

    Don't use absolute path when requiring

--- lib/azure/core/http/retry_policy.rb
+++ lib/azure/core/http/retry_policy.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #--------------------------------------------------------------------------
-require "/azure/core/http/http_filter"
+require "azure/core/http/http_filter"
 
 module Azure
   module Core
