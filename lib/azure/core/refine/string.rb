d41c8fe66b74a79e54edd1f7c82c9f7256597055
A.S. Lomoff <lomov.as@gmail.com>
2013-11-07 17:08:09 +0300

    fix regexp in Azure::Refine::String

--- lib/azure/core/refine/string.rb
+++ lib/azure/core/refine/string.rb
@@ -2,7 +2,7 @@ module Azure::Refine
   class String < String
 
     def camelize
-      self.gsub(/(?:^|[_-\s])([a-z])?/) { $1.upcase unless $1.nil? }.gsub(/\s/, '')
+      self.gsub(/(?:^|[\s_-])([a-z])?/) { $1.upcase unless $1.nil? }.gsub(/\s/, '')
     end
 
   end
