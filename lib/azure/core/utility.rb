aae2e1c7d7db712e1e925d42329a128a4cf63d7c
Ranjan Kumar <ranjankumar188@gmail.com>
2013-11-14 02:07:05 +0530

    PR for virtual network

--- lib/azure/core/utility.rb
+++ lib/azure/core/utility.rb
@@ -13,6 +13,8 @@
 # limitations under the License.
 #--------------------------------------------------------------------------
 
+require 'ipaddr'
+
 if RUBY_VERSION.to_f < 2.0
   begin
     require 'Win32/Console/ANSI' if RUBY_PLATFORM =~ /win32|mingw32/
@@ -144,3 +146,53 @@ class String
   end
 
 end
+
+class IPAddr
+
+  PrivateRanges = [
+    IPAddr.new("10.0.0.0/8"),
+    IPAddr.new("172.16.0.0/12"),
+    IPAddr.new("192.168.0.0/16")
+  ]
+
+  def private?
+    return false unless self.ipv4?
+    PrivateRanges.each do |ipr|
+      return true if ipr.include?(self)
+    end
+    return false
+  end
+
+  def public?
+    !private?
+  end
+
+  class << self
+    def validate_ip_and_prefix(ip, cidr)
+      if cidr.to_s.empty?
+        raise "Cidr is missing for IP '#{ip}'."
+      elsif valid?(ip)
+        raise "Ip address '#{ip}' is invalid."
+      elsif !IPAddr.new(ip).private?
+        raise "Ip Address #{ip} must be private."
+      end
+    end
+
+    def validate_address_space(ip)
+      if ip.split('/').size != 2
+        raise "Cidr is invalid for IP #{ip}."
+      elsif valid?(ip)
+        raise "Address space '#{ip}' is invalid."
+      end
+    end
+
+    def address_prefix(ip, cidr)
+      ip+'/'+cidr.to_s
+    end
+
+    def valid? ip
+      (IPAddr.new(ip) rescue nil).nil?
+    end
+  end
+end
+
