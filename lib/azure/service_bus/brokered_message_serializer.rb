28515919e2c138881e3847bdc919e2d834c658fb
guangyang <allenmacyoung@gmail.com>
2013-11-27 10:15:08 -0800

    fix merge conflict

--- lib/azure/service_bus/brokered_message_serializer.rb
+++ lib/azure/service_bus/brokered_message_serializer.rb
@@ -69,7 +69,7 @@ module Azure
           utc_lock = props['LockedUntilUtc']
           m.locked_until_utc = Time.parse(utc_lock) unless utc_lock.nil?
 
-          enqueued_time_utc = Time.parse(
+          enqueued_time_utc = self.parse_dot_net_serialized_datetime(
             props['EnqueuedTimeUtc']
           ) unless props['EnqueuedTimeUtc'].nil?
           m.enqueued_time_utc = enqueued_time_utc unless enqueued_time_utc.nil?
@@ -132,6 +132,27 @@ module Azure
           hash[name] = tmp[1..(tmp.length - 2)]
         end
         hash
+      end
+
+      private
+
+      # Take the .net json serialization of a DateTime (i.e. /Date(...)/)
+      # and return a time object
+      #
+      # Returns a Time instance
+      def self.parse_dot_net_serialized_datetime(datetime)
+        begin
+          Time.parse(datetime)
+        rescue
+          milliseconds_in_second = 1000
+          match = /\/Date\((\d+)\)\//.match(datetime)
+          if !match.nil?
+            ticks = match[1].to_i
+            Time.at(ticks / milliseconds_in_second)
+          else
+            nil
+          end
+        end
       end
     end
   end
