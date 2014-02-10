ba92e58912694e433fa8349459056a234f4dadf5
Ranjan Kumar <ranjan_kumar@persistent.co.in>
2014-02-10 13:09:47 +0530

    Bug Fix: HTTP 307 Redirection error

--- lib/azure/base_management/management_http_request.rb
+++ lib/azure/base_management/management_http_request.rb
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #--------------------------------------------------------------------------
-require "azure/core/http/http_response"
-require "azure/core/http/http_request"
+require 'azure/core/http/http_response'
+require 'azure/core/http/http_request'
 include Azure::Core::Http
 
 # Represents an HTTP request that can perform synchronous queries to
@@ -21,8 +21,7 @@ include Azure::Core::Http
 module Azure
   module BaseManagement
     class ManagementHttpRequest < HttpRequest
-
-      attr_accessor :uri,:warn,:key,:cert
+      attr_accessor :uri, :warn, :key, :cert
 
       # Public: Creates the ManagementHttpRequest
       #
@@ -31,14 +30,14 @@ module Azure
       # body    - IO or String. The request body (optional)
       # key     - String. The request key
       # cert    - String. The request certificate
-      def initialize(method, path, body=nil)
+      def initialize(method, path, body = nil)
         super
         @warn = false
-        content_length = body ? body.bytesize.to_s : "0"
+        content_length = body ? body.bytesize.to_s : '0'
         @headers = {
-          "x-ms-version" => "2013-06-01",
-          "Content-Type"=> 'application/xml',
-          "Content-Length" => content_length
+          'x-ms-version' => '2013-06-01',
+          'Content-Type' => 'application/xml',
+          'Content-Length' => content_length
         }
         @uri = URI.parse(Azure.config.management_endpoint + Azure.config.subscription_id + path)
         @key = Azure.config.http_private_key
@@ -51,32 +50,10 @@ module Azure
       def call
         request = http_request_class.new(uri.request_uri, headers)
         request.body = body if body
-
-        http = nil
-        if ENV['HTTP_PROXY'] || ENV['HTTPS_PROXY']
-          if ENV['HTTP_PROXY']
-            proxy_uri = URI::parse(ENV['HTTP_PROXY'])
-          else
-            proxy_uri = URI::parse(ENV['HTTPS_PROXY'])
-          end
-
-          http = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new(uri.host, uri.port)
-        else
-          http = Net::HTTP.new(uri.host, uri.port)
-        end
-
-        if uri.scheme.downcase == 'https'
-          http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          http.cert = cert
-          http.key = key
-        end
-        #http.set_debug_output($stdout)
-        response = HttpResponse.new(http.request(request))
-        response.uri = uri
-
-        wait_for_completion(response)
-        Nokogiri::XML response.body
+        http = http_setup
+        # http.set_debug_output($stdout)
+        response = wait_for_completion(HttpResponse.new(http.request(request)))
+        Nokogiri::XML response.body unless response.nil?
       end
 
       # Public: Wait for HTTP request completion.
@@ -91,12 +68,14 @@ module Azure
         if ret_val.at_css('Error Code') && ret_val.at_css('Error Code').content == 'AuthenticationFailed'
           Loggerx.error_with_exit ret_val.at_css('Error Code').content + ' : ' + ret_val.at_css('Error Message').content
         end
-        if (response.status_code.to_i == 200 or response.status_code.to_i == 201)
-          ret_val
+        if response.status_code.to_i == 200 || response.status_code.to_i == 201
+          return response
+        elsif redirected? response
+          rebuild_request response
         elsif response.status_code.to_i > 201 && response.status_code.to_i <= 299
-          ret_val = check_completion(response.headers['x-ms-request-id'])
+          check_completion(response.headers['x-ms-request-id'])
         elsif warn && !response.success?
-          #Loggerx.warn ret_val.at_css('Error Code').content + ' : ' + ret_val.at_css('Error Message').content
+          # Loggerx.warn ret_val.at_css('Error Code').content + ' : ' + ret_val.at_css('Error Message').content
         elsif response.body
           if ret_val.at_css('Error Code') && ret_val.at_css('Error Message')
             Loggerx.error_with_exit ret_val.at_css('Error Code').content + ' : ' + ret_val.at_css('Error Message').content
@@ -106,7 +85,6 @@ module Azure
         else
           Loggerx.exception_message "http error: #{response.status_code}"
         end
-        ret_val
       end
 
       # Public: Gets the status of the specified operation and determines whether
@@ -120,29 +98,74 @@ module Azure
       #
       # Print Error or Success of Operation.
       def check_completion(request_id)
-        request_path= "/operations/#{request_id}"
+        request_path = "/#{Azure.config.subscription_id}/operations/#{request_id}"
+        http = http_setup
+        headers['Content-Length'] = '0'
+        @method = :get
         done = false
         while not done
           print '# '
-          request= ManagementHttpRequest.new(:get, request_path)
-          response = request.call
-          status = xml_content(response, 'Operation Status')
-          status_code = xml_content(response, 'Operation HttpStatusCode')
-          done = status != 'InProgress'
+          request = http_request_class.new(request_path, headers)
+          response = HttpResponse.new(http.request(request))
+          ret_val = Nokogiri::XML response.body
+          status = xml_content(ret_val, 'Operation Status')
+          status_code = response.status_code.to_i
+          if status != 'InProgress'
+            done = true
+          end
+          if redirected? response
+            host_uri = URI.parse(response.headers['location'])
+            http = http_setup(host_uri)
+            done = false
+          end
           if done
-            if status.downcase != "succeeded"
-              error_code = xml_content(response, 'Operation Error Code')
-              error_msg = xml_content(response, 'Operation Error Message')
+            if status.downcase != 'succeeded'
+              error_code = xml_content(ret_val, 'Operation Error Code')
+              error_msg = xml_content(ret_val, 'Operation Error Message')
               Loggerx.exception_message "#{error_code}: #{error_msg}"
             else
-              Loggerx.success " #{status.downcase} (#{status_code})"
+              Loggerx.success "#{status.downcase} (#{status_code})"
             end
-            puts ''
+            return
           else
             sleep(5)
           end
         end
-        response
+      end
+
+      def rebuild_request(response)
+        host_uri = URI.parse(response.headers['location'])
+        request = http_request_class.new(host_uri.request_uri, headers)
+        request.body = body if body
+        http = http_setup(host_uri)
+        wait_for_completion(HttpResponse.new(http.request(request)))
+      end
+
+      def redirected?(response)
+        (response.status_code.to_i == 307)
+      end
+
+      def http_setup(host_uri = nil)
+        @uri = host_uri if host_uri
+        http = nil
+        if ENV['HTTP_PROXY'] || ENV['HTTPS_PROXY']
+          if ENV['HTTP_PROXY']
+            proxy_uri = URI.parse(ENV['HTTP_PROXY'])
+          else
+            proxy_uri = URI.parse(ENV['HTTPS_PROXY'])
+          end
+          http = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new(uri.host, uri.port)
+        else
+          http = Net::HTTP.new(uri.host, uri.port)
+        end
+
+        if uri.scheme.downcase == 'https'
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          http.cert = cert
+          http.key = key
+        end
+        http
       end
     end
   end
