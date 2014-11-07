bff3f77bcc576afe9f62dbdf8aaaa3074483b321
Kononov Dmitriy <kononov.dmitriy@gmail.com>
2014-11-07 12:31:29 +0600

    Update virtual_machine_management_service.rb
    adding new vms types

--- lib/azure/virtual_machine_management/virtual_machine_management_service.rb
+++ lib/azure/virtual_machine_management/virtual_machine_management_service.rb
@@ -529,9 +529,9 @@ module Azure
       end
 
       def validate_role_size(vm_size)
-        valid_role_sizes = %w(ExtraSmall Small Medium Large ExtraLarge A5 A6 A7 Basic_A0 Basic_A1 Basic_A2 Basic_A3 Basic_A4)
+        valid_role_sizes = %w(ExtraSmall Small Medium Large ExtraLarge A5 A6 A7 A8 A9 Standard_D1 Standard_D2 Standard_D3 Standard_D4 Standard_D11 Standard_D12 Standard_D13 Standard_D14 Basic_A0 Basic_A1 Basic_A2 Basic_A3 Basic_A4)
         if vm_size && !valid_role_sizes.include?(vm_size)
-          Loggerx.error_with_exit "Value '#{vm_size}' specified for parameter 'vm_size' is invalid. Allowed values are 'ExtraSmall,Small,Medium,Large,ExtraLarge,A6,A7'"
+          Loggerx.error_with_exit "Value '#{vm_size}' specified for parameter 'vm_size' is invalid. Allowed values are 'ExtraSmall,Small,Medium,Large,ExtraLarge,A5,A6,A7,A8,A9,Standard_D1,Standard_D2,Standard_D3,Standard_D4,Standard_D11,Standard_D12,Standard_D13,Standard_D14,Basic_A0,Basic_A1,Basic_A2,Basic_A3,Basic_A4'"
         end
       end
 
