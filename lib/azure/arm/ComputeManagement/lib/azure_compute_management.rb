8ae3d045a7203129b88528b80f0d4a45ec1975f2
Nick Lebedev <nicklebedev37@gmail.com>
2015-07-24 11:12:14 +0300

    Generated Compute SDK.

---
+++ lib/azure/arm/ComputeManagement/lib/azure_compute_management.rb
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require 'azure_compute_management/module_definition'
+require 'azure_compute_management/version'
+require 'azure_compute_management/sdk_requirements'
