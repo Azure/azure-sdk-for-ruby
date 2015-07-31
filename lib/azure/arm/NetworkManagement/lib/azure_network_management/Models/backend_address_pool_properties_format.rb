e925f597e52f3c99d6205435b98ae2945c4992d2
Nick Lebedev <nicklebedev37@gmail.com>
2015-07-31 12:42:07 +0300

    Regenerated SDK to have fix with namespaces of Azure class in the (de)serialization.

--- lib/azure/arm/NetworkManagement/lib/azure_network_management/Models/backend_address_pool_properties_format.rb
+++ lib/azure/arm/NetworkManagement/lib/azure_network_management/Models/backend_address_pool_properties_format.rb
@@ -47,7 +47,7 @@ module Azure::ARM::Network
           serializedArray = [];
           serialized_property.each do |element|
             if (element)
-              element = Azure::ARM::Network::Models::SubResource.serialize_object(element)
+              element = MsRestAzure::SubResource.serialize_object(element)
             end
             serializedArray.push(element);
           end
@@ -60,7 +60,7 @@ module Azure::ARM::Network
           serializedArray = [];
           serialized_property.each do |element1|
             if (element1)
-              element1 = Azure::ARM::Network::Models::SubResource.serialize_object(element1)
+              element1 = MsRestAzure::SubResource.serialize_object(element1)
             end
             serializedArray.push(element1);
           end
@@ -88,7 +88,7 @@ module Azure::ARM::Network
           deserializedArray = [];
           deserialized_property.each do |element2|
             if (element2)
-              element2 = Azure::ARM::Network::Models::SubResource.deserialize_object(element2)
+              element2 = MsRestAzure::SubResource.deserialize_object(element2)
             end
             deserializedArray.push(element2);
           end
@@ -101,7 +101,7 @@ module Azure::ARM::Network
           deserializedArray = [];
           deserialized_property.each do |element3|
             if (element3)
-              element3 = Azure::ARM::Network::Models::SubResource.deserialize_object(element3)
+              element3 = MsRestAzure::SubResource.deserialize_object(element3)
             end
             deserializedArray.push(element3);
           end
