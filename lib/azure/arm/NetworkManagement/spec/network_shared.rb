ad92d4a193051e83eda51827dd5449f03fd97639
Nick Lebedev <nicklebedev37@gmail.com>
2015-08-11 18:48:21 +0300

    Updated license files to have MIT license.

--- lib/azure/arm/NetworkManagement/spec/network_shared.rb
+++ lib/azure/arm/NetworkManagement/spec/network_shared.rb
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
 def build_virtual_network_params(location)
   params = Models::VirtualNetwork.new
   props = Models::VirtualNetworkPropertiesFormat.new
