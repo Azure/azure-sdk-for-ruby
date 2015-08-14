d6eb458c4ce8f89c026b77f4ff87b4b1a15bad4c
Nick Lebedev <nicklebedev37@gmail.com>
2015-08-14 17:13:56 +0300

    Removed files which retained after renaming.

--- lib/azure/arm/ComputeManagement/lib/azure_compute_management/version.rb
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-
-module Azure::ARM::Compute
-  VERSION = '0.1.0'
-end
