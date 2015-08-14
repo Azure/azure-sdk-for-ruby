02405f9361c59806ec04b8234eb81c01bec9b64b
Nick Lebedev <nicklebedev37@gmail.com>
2015-08-14 17:13:56 +0300

    Removed files which retained after renaming.

--- lib/azure/arm/ResourceManagement/lib/azure_resource_management/version.rb
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-
-module Azure::ARM::Resources
-  VERSION = '0.1.0'
-end
