d850a708f23beee1bbe38f1d3b57c835cb332461
David Justice <david@devigned.com>
2015-10-05 22:39:25 -0700

    add in moar machine sizes

--- resource_management/azure_mgmt_compute/lib/azure_mgmt_compute/models/virtual_machine_size_types.rb
+++ resource_management/azure_mgmt_compute/lib/azure_mgmt_compute/models/virtual_machine_size_types.rb
@@ -9,26 +9,50 @@ module Azure::ARM::Compute
     # Defines values for VirtualMachineSizeTypes
     #
     module VirtualMachineSizeTypes
-      BasicA0 = "Basic_A0"
-      BasicA1 = "Basic_A1"
-      BasicA2 = "Basic_A2"
-      BasicA3 = "Basic_A3"
-      BasicA4 = "Basic_A4"
-      StandardA0 = "Standard_A0"
-      StandardA1 = "Standard_A1"
-      StandardA2 = "Standard_A2"
-      StandardA3 = "Standard_A3"
-      StandardA4 = "Standard_A4"
-      StandardA5 = "Standard_A5"
-      StandardA6 = "Standard_A6"
-      StandardA7 = "Standard_A7"
-      StandardA8 = "Standard_A8"
-      StandardA9 = "Standard_A9"
-      StandardG1 = "Standard_G1"
-      StandardG2 = "Standard_G2"
-      StandardG3 = "Standard_G3"
-      StandardG4 = "Standard_G4"
-      StandardG5 = "Standard_G5"
+      BasicA0 = 'Basic_A0'
+      BasicA1 = 'Basic_A1'
+      BasicA2 = 'Basic_A2'
+      BasicA3 = 'Basic_A3'
+      BasicA4 = 'Basic_A4'
+      StandardA0 = 'Standard_A0'
+      StandardA1 = 'Standard_A1'
+      StandardA2 = 'Standard_A2'
+      StandardA3 = 'Standard_A3'
+      StandardA4 = 'Standard_A4'
+      StandardA5 = 'Standard_A5'
+      StandardA6 = 'Standard_A6'
+      StandardA7 = 'Standard_A7'
+      StandardA8 = 'Standard_A8'
+      StandardA9 = 'Standard_A9'
+      StandardA10 = 'Standard_A10'
+      StandardA11 = 'Standard_A11'
+      StandardD1 = 'Standard_D1'
+      StandardD2 = 'Standard_D2'
+      StandardD3 = 'Standard_D3'
+      StandardD4 = 'Standard_D4'
+      StandardDS1 = 'Standard_DS1'
+      StandardDS2 = 'Standard_DS2'
+      StandardDS3 = 'Standard_DS3'
+      StandardDS4 = 'Standard_DS4'
+      StandardD11 = 'Standard_D11'
+      StandardD12 = 'Standard_D12'
+      StandardD13 = 'Standard_D13'
+      StandardD14 = 'Standard_D14'
+      StandardDS11 = 'Standard_DS11'
+      StandardDS12 = 'Standard_DS12'
+      StandardDS13 = 'Standard_DS13'
+      StandardDS14 = 'Standard_DS14'
+      StandardG1 = 'Standard_G1'
+      StandardG2 = 'Standard_G2'
+      StandardG3 = 'Standard_G3'
+      StandardG4 = 'Standard_G4'
+      StandardG5 = 'Standard_G5'
+      StandardGS1 = 'Standard_GS1'
+      StandardGS2 = 'Standard_GS2'
+      StandardGS3 = 'Standard_GS3'
+      StandardGS4 = 'Standard_GS4'
+      StandardGS5 = 'Standard_GS5'
+
     end
   end
 end
