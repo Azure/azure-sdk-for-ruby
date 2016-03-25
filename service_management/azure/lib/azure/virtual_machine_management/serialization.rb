d81236d84960e6810281e7efd297b1b69b127b86
veronicagg <veronica.giaudrone@microsoft.com>
2016-03-25 11:29:52 -0700

    addressing code review feedback and consolidating code in one method with protocol as a parameter

--- service_management/azure/lib/azure/virtual_machine_management/serialization.rb
+++ service_management/azure/lib/azure/virtual_machine_management/serialization.rb
@@ -94,14 +94,17 @@ module Azure
                 xml.ConfigurationSetType 'NetworkConfiguration'
                 xml.InputEndpoints do
                   default_endpoints_to_xml(xml, options)
-                  tcp_endpoints_to_xml(
+                  specified_endpoints_to_xml(
                     xml,
+                    'TCP',
                     options[:tcp_endpoints],
                     options[:existing_ports]
                   ) if options[:tcp_endpoints]
-                  udp_endpoints_to_xml(
+                  specified_endpoints_to_xml(
                       xml,
-                      options[:udp_endpoints]
+                      'UDP',
+                      options[:udp_endpoints],
+                      options[:existing_ports]
                   ) if options[:udp_endpoints]
                 end
                 if options[:virtual_network_name] && options[:subnet_name]
@@ -229,56 +232,30 @@ module Azure
         endpoints_to_xml(xml, endpoints)
       end
 
-      def self.tcp_endpoints_to_xml(xml, tcp_endpoints, existing_ports = [])
+      def self.specified_endpoints_to_xml(xml, protocol, specified_endpoints, existing_ports = [])
         endpoints = []
+        protocol_up = protocol.upcase
 
-        tcp_endpoints.split(',').each do |endpoint|
+        specified_endpoints.split(',').each do |endpoint|
           ports = endpoint.split(':')
-          tcp_ep = {}
+          specified_ep = {}
 
           if ports.length > 1
             port_already_opened?(existing_ports, ports[1])
 
-            tcp_ep[:name] = "TCP-PORT-#{ports[1]}"
-            tcp_ep[:public_port] = ports[1]
+            specified_ep[:name] = "#{protocol_up}-PORT-#{ports[1]}"
+            specified_ep[:public_port] = ports[1]
           else
             port_already_opened?(existing_ports, ports[0])
 
-            tcp_ep[:name] = "TCP-PORT-#{ports[0]}"
-            tcp_ep[:public_port] = ports[0]
+            specified_ep[:name] = "#{protocol_up}-PORT-#{ports[0]}"
+            specified_ep[:public_port] = ports[0]
           end
 
-          tcp_ep[:local_port] = ports[0]
-          tcp_ep[:protocol] = 'TCP'
+          specified_ep[:local_port] = ports[0]
+          specified_ep[:protocol] = protocol_up
 
-          endpoints << tcp_ep
-        end
-        endpoints_to_xml(xml, endpoints)
-      end
-
-      def self.udp_endpoints_to_xml(xml, udp_endpoints, existing_ports = [])
-        endpoints = []
-
-        udp_endpoints.split(',').each do |endpoint|
-          ports = endpoint.split(':')
-          udp_ep = {}
-
-          if ports.length > 1
-            port_already_opened?(existing_ports, ports[1])
-
-            udp_ep[:name] = "UDP-PORT-#{ports[1]}"
-            udp_ep[:public_port] = ports[1]
-          else
-            port_already_opened?(existing_ports, ports[0])
-
-            udp_ep[:name] = "UDP-PORT-#{ports[0]}"
-            udp_ep[:public_port] = ports[0]
-          end
-
-          udp_ep[:local_port] = ports[0]
-          udp_ep[:protocol] = 'UDP'
-
-          endpoints << udp_ep
+          endpoints << specified_ep
         end
         endpoints_to_xml(xml, endpoints)
       end
