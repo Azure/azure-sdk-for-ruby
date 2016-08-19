bef41b420c76496d74987e2023243844fd006667
Vishrut Shah <vissha@microsoft.com>
2016-08-19 16:08:58 -0700

    test 123

---
+++ azure_mgmt_compute/spec/sample_spec.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require_relative 'spec_helper'
+describe ComputeManagementClient do
+  it 'should create sample samnple set' do
+    expect(ENV['TEST']).to eq('abcd')
+  end
+end
