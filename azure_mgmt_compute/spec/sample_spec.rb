748756aed146fe43919dee5eab9fbe8b5ccfe19c
Vishrut Shah <vissha@microsoft.com>
2016-08-19 16:15:57 -0700

    test 123

--- azure_mgmt_compute/spec/sample_spec.rb
+++ azure_mgmt_compute/spec/sample_spec.rb
@@ -6,6 +6,8 @@ require_relative 'spec_helper'
 describe ComputeManagementClient do
   it 'should create sample samnple set' do
     expect(ENV['TEST']).to eq('abcd')
+  end
+  it 'should create sample set' do
     expect(ENV['TEST123']).to eq('abcd')
   end
 end
