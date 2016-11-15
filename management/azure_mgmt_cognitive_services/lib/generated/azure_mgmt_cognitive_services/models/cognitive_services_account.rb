6ffc55e104103dbe87f8f830b208026521557ae9
Vishrut Shah <vissha@microsoft.com>
2016-11-15 14:23:46 -0800

    Regen azure_mgmt_cognitive_services using AutoRest.1.0.0-Nightly20161114

--- management/azure_mgmt_cognitive_services/lib/generated/azure_mgmt_cognitive_services/models/cognitive_services_account.rb
+++ management/azure_mgmt_cognitive_services/lib/generated/azure_mgmt_cognitive_services/models/cognitive_services_account.rb
@@ -29,8 +29,8 @@ module Azure::ARM::CognitiveServices
       attr_accessor :name
 
       # @return [ProvisioningState] Gets the status of the cognitive services
-      # account at the time the operation was called. Possible values
-      # include: 'Creating', 'ResolvingDNS', 'Succeeded', 'Failed'
+      # account at the time the operation was called. Possible values include:
+      # 'Creating', 'ResolvingDNS', 'Succeeded', 'Failed'
       attr_accessor :provisioning_state
 
       # @return [String] Endpoint of the created account
@@ -39,11 +39,11 @@ module Azure::ARM::CognitiveServices
       # @return [Sku]
       attr_accessor :sku
 
-      # @return [Hash{String => String}] Gets or sets a list of key value
-      # pairs that describe the resource. These tags can be used in viewing
-      # and grouping this resource (across resource groups). A maximum of 15
-      # tags can be provided for a resource. Each tag must have a key no
-      # greater than 128 characters and value no greater than 256 characters.
+      # @return [Hash{String => String}] Gets or sets a list of key value pairs
+      # that describe the resource. These tags can be used in viewing and
+      # grouping this resource (across resource groups). A maximum of 15 tags
+      # can be provided for a resource. Each tag must have a key no greater
+      # than 128 characters and value no greater than 256 characters.
       attr_accessor :tags
 
       # @return [String] Resource type
