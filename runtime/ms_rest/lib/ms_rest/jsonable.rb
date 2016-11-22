e24bc9016f5443c410edf80758c0d20c297f1330
David Justice <david@devigned.com>
2016-11-22 11:33:37 -0800

    add model to_json, from_json and to_s mixin

---
+++ runtime/ms_rest/lib/ms_rest/jsonable.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+module MsRest
+  # Mixin to provide simple serialization / deserialization in AutoRest generated model classes
+  module JSONable
+    include MsRest::Serialization
+
+    #
+    # Serialize the object to JSON
+    # @return [String] JSON serialized version of the object
+    #
+    def to_json
+      serialize(self.class.mapper, self)
+    end
+
+    #
+    # Deserialize the object from JSON
+    # @param json [String] JSON string representation of the object
+    # @return [JSONable] object built from json input
+    #
+    def from_json(json)
+      deserialize(self.class.mapper, json)
+    end
+
+    #
+    # String representation of the object
+    # @return [String]
+    #
+    def to_s
+      "#{super} #{to_json.to_s}"
+    end
+  end
+end
