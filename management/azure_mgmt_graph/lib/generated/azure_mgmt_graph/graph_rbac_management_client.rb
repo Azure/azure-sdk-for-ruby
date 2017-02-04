1c91fda8e2dfeef0d97d6c1b4772f46fd25db887
Vishrut Shah <vissha@microsoft.com>
2017-02-03 18:17:58 -0800

    Regen azure_mgmt_graph using AutoRest.1.0.0-Nightly20170202

--- management/azure_mgmt_graph/lib/generated/azure_mgmt_graph/graph_rbac_management_client.rb
+++ management/azure_mgmt_graph/lib/generated/azure_mgmt_graph/graph_rbac_management_client.rb
@@ -52,12 +52,11 @@ module Azure::ARM::Graph
     # @param base_url [String] the base URI of the service.
     # @param options [Array] filters to be applied to the HTTP requests.
     #
-    def initialize(credentials, base_url = nil, options = nil)
+    def initialize(credentials = nil, base_url = nil, options = nil)
       super(credentials, options)
       @base_url = base_url || 'https://graph.windows.net'
 
-      fail ArgumentError, 'credentials is nil' if credentials.nil?
-      fail ArgumentError, 'invalid type of credentials input parameter' unless credentials.is_a?(MsRest::ServiceClientCredentials)
+      fail ArgumentError, 'invalid type of credentials input parameter' unless credentials.is_a?(MsRest::ServiceClientCredentials) unless credentials.nil?
       @credentials = credentials
 
       @objects = Objects.new(self)
@@ -73,7 +72,7 @@ module Azure::ARM::Graph
 
     #
     # Makes a request and returns the body of the response.
-    # @param method [Symbol] with any of the following values :get, :put, :post, :patch, :delete. 
+    # @param method [Symbol] with any of the following values :get, :put, :post, :patch, :delete.
     # @param path [String] the path, relative to {base_url}.
     # @param options [Hash{String=>String}] specifying any request options like :body.
     # @return [Hash{String=>String}] containing the body of the response.
@@ -134,7 +133,7 @@ module Azure::ARM::Graph
     def add_telemetry
         sdk_information = 'azure_mgmt_graph'
         if defined? Azure::ARM::Graph::VERSION
-          sdk_information = "#{sdk_information}/#{Azure::ARM::Graph::VERSION}" 
+          sdk_information = "#{sdk_information}/#{Azure::ARM::Graph::VERSION}"
         end
         add_user_agent_information(sdk_information)
     end
