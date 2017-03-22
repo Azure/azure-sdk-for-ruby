8c5216e7d9eb80270b31c9483f551cdd2bfe8a9b
Sarangan Rajamanickam <sarajama@microsoft.com>
2017-03-22 09:46:20 -0700

    Fix rspec issue

--- management/azure_mgmt_service_bus/spec/queues_spec.rb
+++ management/azure_mgmt_service_bus/spec/queues_spec.rb
@@ -18,33 +18,33 @@ describe 'Service Bus Management Client' do
   it 'should create list and delete queues' do
     namespace_name = "smaple34566"
     queue_name = 'queue97832'
-    namespace_parameters = Azure::ARM::ServiceBus::Models::NamespaceCreateOrUpdateParameters.new
+    namespace_parameters = Azure::ARM::ServiceBus::Models::Namespace.new
     namespace_parameters.location = 'westus'
-    queue_parameters = Azure::ARM::ServiceBus::Models::QueueCreateOrUpdateParameters.new
+    queue_parameters = Azure::ARM::ServiceBus::Models::Queue.new
     queue_parameters.location = 'westus'
 
     # Create new namespace
-    namespace_resource = @service_bus_client.namespaces.create_or_update(@resource_group.name, namespace_name, namespace_parameters)
-    expect(namespace_resource).not_to be_nil
-    expect(namespace_resource).to be_a(Azure::ARM::ServiceBus::Models::NamespaceResource)
-    expect(namespace_resource.name).to eq(namespace_name)
-    expect(namespace_resource.provisioning_state).to eq("Succeeded")
+    namespace = @service_bus_client.namespaces.create_or_update(@resource_group.name, namespace_name, namespace_parameters)
+    expect(namespace).not_to be_nil
+    expect(namespace).to be_a(Azure::ARM::ServiceBus::Models::Namespace)
+    expect(namespace.name).to eq(namespace_name)
+    expect(namespace.provisioning_state).to eq("Succeeded")
 
     # Create a queue
-    queue_resource = @service_bus_client.queues.create_or_update(@resource_group.name, namespace_name, queue_name, queue_parameters)
-    expect(queue_resource).not_to be_nil
-    expect(queue_resource).to be_a(Azure::ARM::ServiceBus::Models::QueueResource)
-    expect(queue_resource.name).to eq(queue_name)
-    expect(queue_resource.status).to eq("Active")
+    queue = @service_bus_client.queues.create_or_update(@resource_group.name, namespace_name, queue_name, queue_parameters)
+    expect(queue).not_to be_nil
+    expect(queue).to be_a(Azure::ARM::ServiceBus::Models::Queue)
+    expect(queue.name).to eq(queue_name)
+    expect(queue.status).to eq("Active")
 
     # list a queue
-    array_of_queue_resources = @service_bus_client.queues.list_all(@resource_group.name, namespace_name)
-    expect(array_of_queue_resources).not_to be_nil
-    queue_resource = array_of_queue_resources[0]
-    expect(queue_resource).not_to be_nil
-    expect(queue_resource).to be_a(Azure::ARM::ServiceBus::Models::QueueResource)
-    expect(queue_resource.name).to eq(queue_name)
-    expect(queue_resource.status).to eq("Active")
+    array_of_queues = @service_bus_client.queues.list_by_namespace(@resource_group.name, namespace_name)
+    expect(array_of_queues).not_to be_nil
+    queue = array_of_queues[0]
+    expect(queue).not_to be_nil
+    expect(queue).to be_a(Azure::ARM::ServiceBus::Models::Queue)
+    expect(queue.name).to eq(queue_name)
+    expect(queue.status).to eq("Active")
 
     # Delete a queue
     queue = @service_bus_client.queues.delete(@resource_group.name, namespace_name, queue_name)
