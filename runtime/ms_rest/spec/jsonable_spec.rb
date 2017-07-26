afd04f1d66817efa043db95ca866fce91aee4abe
Sarangan Rajamanickam <sarajama@microsoft.com>
2017-07-26 15:01:17 -0700

    Code changes for serialization/deserialization (#860)
    * Code changes for serialization/deserialization
    
    * Added rspec test cases

--- runtime/ms_rest/spec/jsonable_spec.rb
+++ runtime/ms_rest/spec/jsonable_spec.rb
@@ -23,6 +23,22 @@ module MsRest
         {required: false, serialized_name: 'basic', type: { name: 'Composite', class_name: 'Basic', model_properties: {id: {required: false, serialized_name: 'id', type: {name: 'Number'}}, name: {required: false, serialized_name: 'name', type: {name: 'String'}}, color: {required: false, serialized_name: 'color', type: {name: 'String'}}}}}
       end
     end
+
+    class ModifiedBasic
+      include JSONable
+
+      def initialize
+        @identifier = 123
+        @name = 'hello world!'
+        @color = 'blue'
+      end
+
+      attr_accessor :identifier, :name, :color
+
+      def self.mapper
+        {required: false, serialized_name: 'basic', type: { name: 'Composite', class_name: 'ModifiedBasic', model_properties: {id: {required: false, serialized_name: 'id2', type: {name: 'Number'}}, name: {required: false, serialized_name: 'name', type: {name: 'String'}}, color: {required: false, serialized_name: 'color', type: {name: 'String'}}}}}
+      end
+    end
   end
 
   describe Models::Basic do
@@ -41,5 +57,48 @@ module MsRest
     it 'should provide a to_s representation in JSON form with all of the model properties' do
       expect(subject.to_s).to match "#<MsRest::Models::Basic:(.+)> #{variables}"
     end
+
+    it 'should serialize a model using custom mapper' do
+      result = subject.to_json({mapper: MsRest::Models::ModifiedBasic.mapper})
+      expect(result['id2']).to eq 123
+    end
+
+    it 'should serialize a model using custom object without mapper parameter' do
+      bool_body = true
+      current_module = Object.const_get('MsRest::Models')
+      a_new_class = Class.new {
+        include MsRest::JSONable
+        def self.mapper
+          {
+              required: true,
+              serialized_name: 'boolBody',
+              type: {
+                  name: 'Boolean'
+              }
+          }
+        end
+      }
+      current_module.const_set('AnotherNewClass', a_new_class)
+      obj = a_new_class.new
+      request_content = obj.to_json({object: bool_body })
+      expect(request_content).to be_truthy
+    end
+
+    it 'should serialize using a non composite model' do
+      request_mapper = {
+          required: true,
+          serialized_name: 'boolBody',
+          type: {
+              name: 'Boolean'
+          }
+      }
+      bool_body = true
+      current_module = Object.const_get('MsRest::Models')
+      a_new_class = Class.new { include MsRest::JSONable }
+      current_module.const_set('ANewClass', a_new_class)
+      request_content = a_new_class.new.to_json({mapper: request_mapper, object: bool_body })
+      expect(request_content).to be_truthy
+    end
+
   end
 end
