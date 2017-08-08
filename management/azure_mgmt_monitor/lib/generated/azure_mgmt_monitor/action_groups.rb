03093ca083ffc7ec307c8a63fa836be62410c8c3
SwaggerToSDK Automation <autorestci@microsoft.com>
2017-08-08 21:22:06 +0000

    Generated from d95a4cea748e3321614a7fca5536073c1f9f29a2

--- management/azure_mgmt_monitor/lib/generated/azure_mgmt_monitor/action_groups.rb
+++ management/azure_mgmt_monitor/lib/generated/azure_mgmt_monitor/action_groups.rb
@@ -206,7 +206,7 @@ module Azure::ARM::Monitor
         http_response = result.response
         status_code = http_response.status
         response_content = http_response.body
-        unless status_code == 200 || status_code == 404
+        unless status_code == 200
           error_model = JSON.load(response_content)
           fail MsRestAzure::AzureOperationError.new(result.request, http_response, error_model)
         end
@@ -562,7 +562,7 @@ module Azure::ARM::Monitor
         http_response = result.response
         status_code = http_response.status
         response_content = http_response.body
-        unless status_code == 200 || status_code == 409 || status_code == 404
+        unless status_code == 200
           error_model = JSON.load(response_content)
           fail MsRestAzure::AzureOperationError.new(result.request, http_response, error_model)
         end
