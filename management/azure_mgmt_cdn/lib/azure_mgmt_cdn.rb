41c5d3d4204ef05c418b12e74fbe69f00adcab76
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2017-09-14 18:43:40 -0700

    manual updates to azure_mgmt_cdn

--- management/azure_mgmt_cdn/lib/azure_mgmt_cdn.rb
+++ management/azure_mgmt_cdn/lib/azure_mgmt_cdn.rb
@@ -2,4 +2,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require 'generated/azure_mgmt_cdn'
+require '2016-10-02/generated/azure_mgmt_cdn'
