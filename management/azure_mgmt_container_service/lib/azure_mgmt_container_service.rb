35c59647d50da5944f567a62810df309fc7715a3
Veronica Giaudrone <veronicagg@users.noreply.github.com>
2017-09-15 15:30:00 -0700

    adding/updating container services in a separate gem

---
+++ management/azure_mgmt_container_service/lib/azure_mgmt_container_service.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '2017-01-31/generated/azure_mgmt_compute'
