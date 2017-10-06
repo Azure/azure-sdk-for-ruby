8e9cb078342db6e052413881f6c9f3da6a2b6979
Sarangan Rajamanickam <sarajama@microsoft.com>
2017-10-06 11:12:02 -0700

    Removed everything related to api management

--- management/azure_mgmt_api_management/lib/azure_mgmt_api_management.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-
-require '2017-03-01/generated/azure_mgmt_api_management'
-# Adding require for the profiles
-require 'profiles/Profile_2017_03_01/apimanagement_profile_2017_03_01_profile_client'
-require 'profiles/Profile_Latest/apimanagement_profile_latest_profile_client'
