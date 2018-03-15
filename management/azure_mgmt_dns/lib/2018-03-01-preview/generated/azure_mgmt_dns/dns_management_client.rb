f4b51e615744ee84d6f4dad81893a85006aa61c7
Sarangan Rajamanickam <sarajama@microsoft.com>
2018-03-15 13:53:38 -0700

    Update version number inside azure_mgmt_dns

--- management/azure_mgmt_dns/lib/2018-03-01-preview/generated/azure_mgmt_dns/dns_management_client.rb
+++ management/azure_mgmt_dns/lib/2018-03-01-preview/generated/azure_mgmt_dns/dns_management_client.rb
@@ -128,7 +128,7 @@ module Azure::Dns::Mgmt::V2018_03_01_preview
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_dns'
-        sdk_information = "#{sdk_information}/0.16.0"
+        sdk_information = "#{sdk_information}/0.16.1"
         add_user_agent_information(sdk_information)
     end
   end
