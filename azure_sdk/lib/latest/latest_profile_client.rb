d8b378d251d76e73489215d3502744d500cc9372
Sarangan Rajamanickam <sarajama@microsoft.com>
2018-03-19 22:33:30 -0700

    Regenerated SDK and profiles

--- azure_sdk/lib/latest/latest_profile_client.rb
+++ azure_sdk/lib/latest/latest_profile_client.rb
@@ -4,6 +4,7 @@
 
 require 'latest/module_definition'
 require 'latest/modules/analysisservices_profile_module'
+require 'latest/modules/apimanagement_profile_module'
 require 'latest/modules/authorization_profile_module'
 require 'latest/modules/automation_profile_module'
 require 'latest/modules/batch_profile_module'
@@ -80,7 +81,7 @@ module Azure::Profiles::Latest
   class Client
     include MsRestAzure::Common::Configurable
 
-    attr_reader  :analysis_services, :authorization, :automation, :batch, :billing, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :customer_insights, :data_lake_analytics, :data_lake_store, :dev_test_labs, :dns, :event_grid, :event_hub, :features, :graph_rbac, :iot_hub, :key_vault, :links, :locks, :logic, :machine_learning, :managed_applications, :marketplace_ordering, :media_services, :mobile_engagement, :monitor, :managed_service_identity, :network, :notification_hubs, :operational_insights, :policy, :power_bi_embedded, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :resources, :resources_management, :scheduler, :search, :server_management, :service_bus, :service_fabric, :sql, :stor_simple8000_series, :storage, :stream_analytics, :subscriptions, :traffic_manager, :web, :computer_vision, :content_moderator, :custom_search, :entity_search, :face, :image_search, :news_search, :spell_check, :text_analytics, :video_search, :web_search
+    attr_reader  :analysis_services, :api_management, :authorization, :automation, :batch, :billing, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :customer_insights, :data_lake_analytics, :data_lake_store, :dev_test_labs, :dns, :event_grid, :event_hub, :features, :graph_rbac, :iot_hub, :key_vault, :links, :locks, :logic, :machine_learning, :managed_applications, :marketplace_ordering, :media_services, :mobile_engagement, :monitor, :managed_service_identity, :network, :notification_hubs, :operational_insights, :policy, :power_bi_embedded, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :resources, :resources_management, :scheduler, :search, :server_management, :service_bus, :service_fabric, :sql, :stor_simple8000_series, :storage, :stream_analytics, :subscriptions, :traffic_manager, :web, :computer_vision, :content_moderator, :custom_search, :entity_search, :face, :image_search, :news_search, :spell_check, :text_analytics, :video_search, :web_search
 
     #
     # Initializes a new instance of the Client class.
@@ -113,6 +114,7 @@ module Azure::Profiles::Latest
       sdk_options = options[:options].nil? ? nil:options[:options]
 
       @analysis_services = AnalysisServicesAdapter.new(self, base_url, sdk_options)
+      @api_management = ApiManagementAdapter.new(self, base_url, sdk_options)
       @authorization = AuthorizationAdapter.new(self, base_url, sdk_options)
       @automation = AutomationAdapter.new(self, base_url, sdk_options)
       @batch = BatchAdapter.new(self, base_url, sdk_options)
@@ -188,6 +190,14 @@ module Azure::Profiles::Latest
 
       def initialize(context, base_url, options)
         @mgmt = Azure::Profiles::Latest::AnalysisServices::Mgmt::AnalysisServicesManagementClass.new(context, base_url, options)
+      end
+    end
+
+    class ApiManagementAdapter
+      attr_accessor :mgmt
+
+      def initialize(context, base_url, options)
+        @mgmt = Azure::Profiles::Latest::ApiManagement::Mgmt::ApiManagementManagementClass.new(context, base_url, options)
       end
     end
 
