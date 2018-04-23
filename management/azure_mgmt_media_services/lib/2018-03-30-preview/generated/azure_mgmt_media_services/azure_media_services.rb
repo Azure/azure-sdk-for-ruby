70c0f31c51955acb52bec65af06ef181f01377c4
Sarangan Rajamanickam <sarajama@microsoft.com>
2018-04-23 13:25:44 -0700

    Updated Changelog, versions and regened all

--- management/azure_mgmt_media_services/lib/2018-03-30-preview/generated/azure_mgmt_media_services/azure_media_services.rb
+++ management/azure_mgmt_media_services/lib/2018-03-30-preview/generated/azure_mgmt_media_services/azure_media_services.rb
@@ -169,7 +169,7 @@ module Azure::MediaServices::Mgmt::V2018_03_30_preview
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_media_services'
-        sdk_information = "#{sdk_information}/0.16.0"
+        sdk_information = "#{sdk_information}/0.16.1"
         add_user_agent_information(sdk_information)
     end
   end
