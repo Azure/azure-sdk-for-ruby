93c1bf1c069b316f20ab8f2520e303dd9941162a
Sarangan Rajamanickam <sarajama@microsoft.com>
2018-11-05 14:49:48 -0800

    Regen all SDks & Profiles

--- azure_sdk/lib/azure_sdk/version.rb
+++ azure_sdk/lib/azure_sdk/version.rb
@@ -3,5 +3,5 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
 module Azure
-  VERSION = '0.18.9'
+  VERSION = '0.18.10'
 end
--- management/azure_mgmt_data_migration/lib/azure_mgmt_data_migration.rb
+++ management/azure_mgmt_data_migration/lib/azure_mgmt_data_migration.rb
@@ -2,3 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
+require '2018-07-15-preview/generated/azure_mgmt_data_migration'
+require '2018-04-19/generated/azure_mgmt_data_migration'
+require '2018-03-31-preview/generated/azure_mgmt_data_migration'
+require '2017-11-15-preview/generated/azure_mgmt_data_migration'
+require '2018-03-15-preview/generated/azure_mgmt_data_migration'
+require 'profiles/latest/datamigration_latest_profile_client'
