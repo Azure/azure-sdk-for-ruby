7d25541344a840b71c4d7b5658ef2dd02c024bc4
Sarangan Rajamanickam <sarajama@microsoft.com>
2018-11-06 10:56:38 -0800

    Regen all SDKs & Profiles

--- data/azure_cognitiveservices_websearch/lib/1.0/generated/azure_cognitiveservices_websearch/web_search_client.rb
+++ data/azure_cognitiveservices_websearch/lib/1.0/generated/azure_cognitiveservices_websearch/web_search_client.rb
@@ -121,7 +121,7 @@ module Azure::CognitiveServices::WebSearch::V1_0
     #
     def add_telemetry
         sdk_information = 'azure_cognitiveservices_websearch'
-        sdk_information = "#{sdk_information}/0.17.0"
+        sdk_information = "#{sdk_information}/0.17.1"
         add_user_agent_information(sdk_information)
     end
   end
