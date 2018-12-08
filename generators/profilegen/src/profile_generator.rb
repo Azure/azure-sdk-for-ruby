94b369c6e4d4ae1fc21db41e9ba7fc2c37b4adcc
Sarangan Rajamanickam <sarajama@microsoft.com>
2018-12-07 17:54:03 -0800

    Regen all SDKs & Profiles

--- generators/profilegen/src/profile_generator.rb
+++ generators/profilegen/src/profile_generator.rb
@@ -14,7 +14,7 @@ require_relative 'defs/module_definition_def'
 # Class to generate the profile
 #
 class ProfileGenerator
-  CLIENTS_WITH_NO_BASE_URL = %w{ComputerVisionDataClass ContentModeratorDataClass FaceDataClass TextAnalyticsDataClass EventGridDataClass KeyVaultDataClass QnamakerDataClass CustomvisionpredictionDataClass CustomvisiontrainingDataClass AutosuggestDataClass CustomimagesearchDataClass }
+  CLIENTS_WITH_NO_BASE_URL = %w{ComputerVisionDataClass ContentModeratorDataClass FaceDataClass TextAnalyticsDataClass EventGridDataClass KeyVaultDataClass QnamakerDataClass CustomvisionpredictionDataClass CustomvisiontrainingDataClass AutosuggestDataClass CustomimagesearchDataClass CustomSearchDataClass}
 
   #
   # Constructor for the profile generator.
