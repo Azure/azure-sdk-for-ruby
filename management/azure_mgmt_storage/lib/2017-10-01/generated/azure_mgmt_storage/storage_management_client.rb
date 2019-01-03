6f500bbd9fdf66c3bfb094381f474793a74866ca
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-01-02 23:09:17 -0800

    Regen all SDKs & Profiles

--- management/azure_mgmt_storage/lib/2017-10-01/generated/azure_mgmt_storage/storage_management_client.rb
+++ management/azure_mgmt_storage/lib/2017-10-01/generated/azure_mgmt_storage/storage_management_client.rb
@@ -138,7 +138,7 @@ module Azure::Storage::Mgmt::V2017_10_01
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_storage'
-        sdk_information = "#{sdk_information}/0.17.4"
+        sdk_information = "#{sdk_information}/0.17.5"
         add_user_agent_information(sdk_information)
     end
   end
