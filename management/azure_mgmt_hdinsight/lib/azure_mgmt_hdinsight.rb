990a924e33d4d3887c46dfeadb1dc89537a917b2
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-01-14 12:22:27 -0800

    Initial Config changes for release

---
+++ management/azure_mgmt_hdinsight/lib/azure_mgmt_hdinsight.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '2018-06-01-preview/generated/azure_mgmt_hdinsight'
+require '2015-03-01-preview/generated/azure_mgmt_hdinsight'
+require 'profiles/latest/hdinsight_latest_profile_client'
