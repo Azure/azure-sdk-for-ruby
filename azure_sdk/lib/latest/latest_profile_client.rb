29d59d5c29eddb2034a938f02ed9e044c78ac162
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-02-26 13:42:18 -0800

    Regen all SDKs & Profiles

--- azure_sdk/lib/latest/latest_profile_client.rb
+++ azure_sdk/lib/latest/latest_profile_client.rb
@@ -24,6 +24,7 @@ require 'latest/modules/datamigration_profile_module'
 require 'latest/modules/devspaces_profile_module'
 require 'latest/modules/devtestlabs_profile_module'
 require 'latest/modules/dns_profile_module'
+require 'latest/modules/edgegateway_profile_module'
 require 'latest/modules/eventgrid_profile_module'
 require 'latest/modules/eventhub_profile_module'
 require 'latest/modules/features_profile_module'
@@ -100,7 +101,7 @@ module Azure::Profiles::Latest
   class Client
     include MsRestAzure::Common::Configurable
 
-    attr_reader  :analysis_services, :api_management, :authorization, :automation, :batch, :billing, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :customer_insights, :data_lake_analytics, :data_lake_store, :data_migration, :dev_spaces, :dev_test_labs, :dns, :event_grid, :event_hub, :features, :hdinsight, :graph_rbac, :iot_central, :iot_hub, :key_vault, :kusto, :labservices, :links, :locks, :logic, :machine_learning, :machine_learning_services, :managed_applications, :marketplace_ordering, :media_services, :monitor, :managed_service_identity, :network, :notification_hubs, :operational_insights, :policy, :policy_insights, :power_bi_embedded, :private_dns, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :resources, :resources_management, :scheduler, :search, :security, :service_bus, :service_fabric, :signalr, :sql, :sqlvirtualmachine, :stor_simple8000_series, :storage, :stream_analytics, :subscriptions, :traffic_manager, :web, :autosuggest, :customimagesearch, :computer_vision, :content_moderator, :custom_search, :customvisiontraining, :customvisionprediction, :entity_search, :face, :image_search, :local_search, :luis_runtime, :luis_authoring, :news_search, :qnamaker, :spell_check, :text_analytics, :video_search, :web_search, :visual_search
+    attr_reader  :analysis_services, :api_management, :authorization, :automation, :batch, :billing, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :customer_insights, :data_lake_analytics, :data_lake_store, :data_migration, :dev_spaces, :dev_test_labs, :dns, :edge_gateway, :event_grid, :event_hub, :features, :hdinsight, :graph_rbac, :iot_central, :iot_hub, :key_vault, :kusto, :labservices, :links, :locks, :logic, :machine_learning, :machine_learning_services, :managed_applications, :marketplace_ordering, :media_services, :monitor, :managed_service_identity, :network, :notification_hubs, :operational_insights, :policy, :policy_insights, :power_bi_embedded, :private_dns, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :resources, :resources_management, :scheduler, :search, :security, :service_bus, :service_fabric, :signalr, :sql, :sqlvirtualmachine, :stor_simple8000_series, :storage, :stream_analytics, :subscriptions, :traffic_manager, :web, :autosuggest, :customimagesearch, :computer_vision, :content_moderator, :custom_search, :customvisiontraining, :customvisionprediction, :entity_search, :face, :image_search, :local_search, :luis_runtime, :luis_authoring, :news_search, :qnamaker, :spell_check, :text_analytics, :video_search, :web_search, :visual_search
 
     #
     # Initializes a new instance of the Client class.
@@ -153,6 +154,7 @@ module Azure::Profiles::Latest
       @dev_spaces = DevSpacesAdapter.new(self, base_url, sdk_options)
       @dev_test_labs = DevTestLabsAdapter.new(self, base_url, sdk_options)
       @dns = DnsAdapter.new(self, base_url, sdk_options)
+      @edge_gateway = EdgeGatewayAdapter.new(self, base_url, sdk_options)
       @event_grid = EventGridAdapter.new(self, base_url, sdk_options)
       @event_hub = EventHubAdapter.new(self, base_url, sdk_options)
       @features = FeaturesAdapter.new(self, base_url, sdk_options)
@@ -388,6 +390,14 @@ module Azure::Profiles::Latest
 
       def initialize(context, base_url, options)
         @mgmt = Azure::Profiles::Latest::Dns::Mgmt::DnsManagementClass.new(context, base_url, options)
+      end
+    end
+
+    class EdgeGatewayAdapter
+      attr_accessor :mgmt
+
+      def initialize(context, base_url, options)
+        @mgmt = Azure::Profiles::Latest::EdgeGateway::Mgmt::EdgeGatewayManagementClass.new(context, base_url, options)
       end
     end
 
--- management/azure_mgmt_edgegateway/lib/profiles/latest/edgegateway_module_definition.rb
+++ management/azure_mgmt_edgegateway/lib/profiles/latest/edgegateway_module_definition.rb
@@ -2,5 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require '2019-03-01/generated/azure_mgmt_edgegateway'
-require 'profiles/latest/edgegateway_latest_profile_client'
+module Azure end
+module Azure::EdgeGateway end
+module Azure::EdgeGateway::Profiles end
+module Azure::EdgeGateway::Profiles::Latest end
