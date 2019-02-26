e1777b2000067e14ac249998a8f498fb7dea0513
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-02-26 14:07:53 -0800

    Regen all SDKs & Profiles

--- azure_sdk/lib/latest/latest_profile_client.rb
+++ azure_sdk/lib/latest/latest_profile_client.rb
@@ -43,6 +43,7 @@ require 'latest/modules/machinelearningservices_profile_module'
 require 'latest/modules/managedapplications_profile_module'
 require 'latest/modules/marketplaceordering_profile_module'
 require 'latest/modules/mediaservices_profile_module'
+require 'latest/modules/mixedreality_profile_module'
 require 'latest/modules/monitor_profile_module'
 require 'latest/modules/managedserviceidentity_profile_module'
 require 'latest/modules/network_profile_module'
@@ -101,7 +102,7 @@ module Azure::Profiles::Latest
   class Client
     include MsRestAzure::Common::Configurable
 
-    attr_reader  :analysis_services, :api_management, :authorization, :automation, :batch, :billing, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :customer_insights, :data_lake_analytics, :data_lake_store, :data_migration, :dev_spaces, :dev_test_labs, :dns, :edge_gateway, :event_grid, :event_hub, :features, :hdinsight, :graph_rbac, :iot_central, :iot_hub, :key_vault, :kusto, :labservices, :links, :locks, :logic, :machine_learning, :machine_learning_services, :managed_applications, :marketplace_ordering, :media_services, :monitor, :managed_service_identity, :network, :notification_hubs, :operational_insights, :policy, :policy_insights, :power_bi_embedded, :private_dns, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :resources, :resources_management, :scheduler, :search, :security, :service_bus, :service_fabric, :signalr, :sql, :sqlvirtualmachine, :stor_simple8000_series, :storage, :stream_analytics, :subscriptions, :traffic_manager, :web, :autosuggest, :customimagesearch, :computer_vision, :content_moderator, :custom_search, :customvisiontraining, :customvisionprediction, :entity_search, :face, :image_search, :local_search, :luis_runtime, :luis_authoring, :news_search, :qnamaker, :spell_check, :text_analytics, :video_search, :web_search, :visual_search
+    attr_reader  :analysis_services, :api_management, :authorization, :automation, :batch, :billing, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :customer_insights, :data_lake_analytics, :data_lake_store, :data_migration, :dev_spaces, :dev_test_labs, :dns, :edge_gateway, :event_grid, :event_hub, :features, :hdinsight, :graph_rbac, :iot_central, :iot_hub, :key_vault, :kusto, :labservices, :links, :locks, :logic, :machine_learning, :machine_learning_services, :managed_applications, :marketplace_ordering, :media_services, :mixed_reality, :monitor, :managed_service_identity, :network, :notification_hubs, :operational_insights, :policy, :policy_insights, :power_bi_embedded, :private_dns, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :resources, :resources_management, :scheduler, :search, :security, :service_bus, :service_fabric, :signalr, :sql, :sqlvirtualmachine, :stor_simple8000_series, :storage, :stream_analytics, :subscriptions, :traffic_manager, :web, :autosuggest, :customimagesearch, :computer_vision, :content_moderator, :custom_search, :customvisiontraining, :customvisionprediction, :entity_search, :face, :image_search, :local_search, :luis_runtime, :luis_authoring, :news_search, :qnamaker, :spell_check, :text_analytics, :video_search, :web_search, :visual_search
 
     #
     # Initializes a new instance of the Client class.
@@ -173,6 +174,7 @@ module Azure::Profiles::Latest
       @managed_applications = ManagedApplicationsAdapter.new(self, base_url, sdk_options)
       @marketplace_ordering = MarketplaceOrderingAdapter.new(self, base_url, sdk_options)
       @media_services = MediaServicesAdapter.new(self, base_url, sdk_options)
+      @mixed_reality = MixedRealityAdapter.new(self, base_url, sdk_options)
       @monitor = MonitorAdapter.new(self, base_url, sdk_options)
       @managed_service_identity = ManagedServiceIdentityAdapter.new(self, base_url, sdk_options)
       @network = NetworkAdapter.new(self, base_url, sdk_options)
@@ -543,6 +545,14 @@ module Azure::Profiles::Latest
 
       def initialize(context, base_url, options)
         @mgmt = Azure::Profiles::Latest::MediaServices::Mgmt::MediaServicesManagementClass.new(context, base_url, options)
+      end
+    end
+
+    class MixedRealityAdapter
+      attr_accessor :mgmt
+
+      def initialize(context, base_url, options)
+        @mgmt = Azure::Profiles::Latest::MixedReality::Mgmt::MixedRealityManagementClass.new(context, base_url, options)
       end
     end
 
--- management/azure_mgmt_mixedreality/lib/profiles/latest/mixedreality_module_definition.rb
+++ management/azure_mgmt_mixedreality/lib/profiles/latest/mixedreality_module_definition.rb
@@ -2,5 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require '2019-02-28-preview/generated/azure_mgmt_mixedreality'
-require 'profiles/latest/mixedreality_latest_profile_client'
+module Azure end
+module Azure::MixedReality end
+module Azure::MixedReality::Profiles end
+module Azure::MixedReality::Profiles::Latest end
