6e20c080f0b76f04f061476d8bf1021d22f82dbc
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-03-04 13:49:11 -0800

    Code changes for netapp

--- management/azure_mgmt_netapp/lib/profiles/latest/netapp_module_definition.rb
+++ management/azure_mgmt_netapp/lib/profiles/latest/netapp_module_definition.rb
@@ -2,5 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require '2017-08-15-preview/generated/azure_mgmt_netapp'
-require 'profiles/latest/netapp_latest_profile_client'
+module Azure end
+module Azure::NetApp end
+module Azure::NetApp::Profiles end
+module Azure::NetApp::Profiles::Latest end
