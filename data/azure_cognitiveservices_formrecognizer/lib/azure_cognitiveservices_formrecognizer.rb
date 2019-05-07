2d7cd92aa5ca6079c09c3b01a339a131990d7640
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-05-07 14:53:45 -0700

    Code changes for Form Recognizer

---
+++ data/azure_cognitiveservices_formrecognizer/lib/azure_cognitiveservices_formrecognizer.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '1.0-preview/generated/azure_cognitiveservices_formrecognizer'
+require 'profiles/latest/formrecognizer_latest_profile_client'
