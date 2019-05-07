125d535de7764be1f5c317883b3f3d5889fbeeb5
Sarangan Rajamanickam <sarajama@microsoft.com>
2019-05-07 15:33:08 -0700

    Minor changes

--- azure_sdk/lib/latest/modules/formrecognizer_profile_module.rb
+++ azure_sdk/lib/latest/modules/formrecognizer_profile_module.rb
@@ -32,7 +32,7 @@ module Azure::Profiles::Latest
       def initialize(configurable, base_url=nil, options=nil)
         @configurable, @base_url, @options = configurable, base_url, options
 
-        @client_0 = Azure::CognitiveServices::FormRecognizer::V1_0_preview::FormRecognizerClient.new(configurable.credentials, base_url, options)
+        @client_0 = Azure::CognitiveServices::FormRecognizer::V1_0_preview::FormRecognizerClient.new(configurable.credentials, options)
         if(@client_0.respond_to?(:subscription_id))
           @client_0.subscription_id = configurable.subscription_id
         end
