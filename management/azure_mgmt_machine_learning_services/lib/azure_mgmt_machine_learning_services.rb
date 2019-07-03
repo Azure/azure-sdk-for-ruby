effe201a3e488c4bce20b3c39574dce2061b83b3
MyronFanQiu <fanqiu@microsoft.com>
2019-07-03 16:12:19 +0800

    Regen for Cognitive Service, Machine Learning Service and AppConfiguration Services

--- management/azure_mgmt_machine_learning_services/lib/azure_mgmt_machine_learning_services.rb
+++ management/azure_mgmt_machine_learning_services/lib/azure_mgmt_machine_learning_services.rb
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require '2019-05-01/generated/azure_mgmt_machine_learning_services'
 require '2018-11-19/generated/azure_mgmt_machine_learning_services'
 require '2018-03-01-preview/generated/azure_mgmt_machine_learning_services'
+require '2019-05-01/generated/azure_mgmt_machine_learning_services'
 require 'profiles/latest/machinelearningservices_latest_profile_client'
