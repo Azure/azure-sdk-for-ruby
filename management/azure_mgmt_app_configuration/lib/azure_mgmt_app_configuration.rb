9d80629bfc942c7435e4c8744fb7983311c84682
MyronFanQiu <fanqiu@microsoft.com>
2019-07-05 16:11:20 +0800

    Regen SDK for azure mgmt app configuration

---
+++ management/azure_mgmt_app_configuration/lib/azure_mgmt_app_configuration.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '2019-02-01-preview/generated/azure_mgmt_app_configuration'
+require 'profiles/latest/appconfiguration_latest_profile_client'
