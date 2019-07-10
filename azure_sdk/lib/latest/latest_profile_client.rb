c2cda6462bccb8cdc0deff23f8b29724c136d22e
MyronFanQiu <fanqiu@microsoft.com>
2019-07-10 19:18:20 +0800

    Regen SDK for migrate

--- azure_sdk/lib/latest/latest_profile_client.rb
+++ azure_sdk/lib/latest/latest_profile_client.rb
@@ -56,6 +56,7 @@ require 'latest/modules/managedapplications_profile_module'
 require 'latest/modules/mariadb_profile_module'
 require 'latest/modules/marketplaceordering_profile_module'
 require 'latest/modules/mediaservices_profile_module'
+require 'latest/modules/migrate_profile_module'
 require 'latest/modules/mixedreality_profile_module'
 require 'latest/modules/monitor_profile_module'
 require 'latest/modules/managedserviceidentity_profile_module'
@@ -125,7 +126,7 @@ module Azure::Profiles::Latest
   class Client
     include MsRestAzure::Common::Configurable
 
-    attr_reader  :adhybrid_health_service, :advisor, :alerts_management, :analysis_services, :api_management, :app_configuration, :authorization, :automation, :azure_stack, :batch, :batch_ai, :billing, :bot_service, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :cosmosdb, :cost_management, :customer_insights, :data_box, :data_lake_analytics, :data_lake_store, :data_migration, :data_factory, :dev_spaces, :dev_test_labs, :dns, :edge_gateway, :event_grid, :event_hub, :features, :hanaonazure, :hdinsight, :graph_rbac, :iot_central, :iot_hub, :key_vault, :kusto, :labservices, :links, :locks, :logic, :machine_learning, :machine_learning_services, :managed_applications, :maria_db, :marketplace_ordering, :media_services, :mixed_reality, :monitor, :managed_service_identity, :net_app, :network, :notification_hubs, :operational_insights, :policy, :policy_insights, :postgresql, :power_bi_embedded, :private_dns, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :reservations, :resource_graph, :resources, :resources_management, :scheduler, :search, :security, :serialconsole, :service_bus, :service_fabric, :signalr, :sql, :sqlvirtualmachine, :stor_simple8000_series, :storage, :storage_sync, :stream_analytics, :subscriptions, :traffic_manager, :web, :anomaly_detector, :autosuggest, :customimagesearch, :computer_vision, :content_moderator, :custom_search, :customvisiontraining, :customvisionprediction, :entity_search, :face, :form_recognizer, :image_search, :local_search, :luis_runtime, :luis_authoring, :news_search, :personalizer, :qnamaker, :spell_check, :text_analytics, :video_search, :web_search, :visual_search
+    attr_reader  :adhybrid_health_service, :advisor, :alerts_management, :analysis_services, :api_management, :app_configuration, :authorization, :automation, :azure_stack, :batch, :batch_ai, :billing, :bot_service, :cdn, :cognitive_services, :commerce, :compute, :consumption, :container_instance, :container_registry, :container_service, :cosmosdb, :cost_management, :customer_insights, :data_box, :data_lake_analytics, :data_lake_store, :data_migration, :data_factory, :dev_spaces, :dev_test_labs, :dns, :edge_gateway, :event_grid, :event_hub, :features, :hanaonazure, :hdinsight, :graph_rbac, :iot_central, :iot_hub, :key_vault, :kusto, :labservices, :links, :locks, :logic, :machine_learning, :machine_learning_services, :managed_applications, :maria_db, :marketplace_ordering, :media_services, :migrate, :mixed_reality, :monitor, :managed_service_identity, :net_app, :network, :notification_hubs, :operational_insights, :policy, :policy_insights, :postgresql, :power_bi_embedded, :private_dns, :recovery_services, :recovery_services_backup, :recovery_services_site_recovery, :redis, :relay, :reservations, :resource_graph, :resources, :resources_management, :scheduler, :search, :security, :serialconsole, :service_bus, :service_fabric, :signalr, :sql, :sqlvirtualmachine, :stor_simple8000_series, :storage, :storage_sync, :stream_analytics, :subscriptions, :traffic_manager, :web, :anomaly_detector, :autosuggest, :customimagesearch, :computer_vision, :content_moderator, :custom_search, :customvisiontraining, :customvisionprediction, :entity_search, :face, :form_recognizer, :image_search, :local_search, :luis_runtime, :luis_authoring, :news_search, :personalizer, :qnamaker, :spell_check, :text_analytics, :video_search, :web_search, :visual_search
 
     #
     # Initializes a new instance of the Client class.
@@ -210,6 +211,7 @@ module Azure::Profiles::Latest
       @maria_db = MariaDBAdapter.new(self, base_url, sdk_options)
       @marketplace_ordering = MarketplaceOrderingAdapter.new(self, base_url, sdk_options)
       @media_services = MediaServicesAdapter.new(self, base_url, sdk_options)
+      @migrate = MigrateAdapter.new(self, base_url, sdk_options)
       @mixed_reality = MixedRealityAdapter.new(self, base_url, sdk_options)
       @monitor = MonitorAdapter.new(self, base_url, sdk_options)
       @managed_service_identity = ManagedServiceIdentityAdapter.new(self, base_url, sdk_options)
@@ -695,6 +697,14 @@ module Azure::Profiles::Latest
 
       def initialize(context, base_url, options)
         @mgmt = Azure::Profiles::Latest::MediaServices::Mgmt::MediaServicesManagementClass.new(context, base_url, options)
+      end
+    end
+
+    class MigrateAdapter
+      attr_accessor :mgmt
+
+      def initialize(context, base_url, options)
+        @mgmt = Azure::Profiles::Latest::Migrate::Mgmt::MigrateManagementClass.new(context, base_url, options)
       end
     end
 
---
+++ management/azure_mgmt_migrate/lib/azure_mgmt_migrate.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '2018-02-02/generated/azure_mgmt_migrate'
+require 'profiles/latest/migrate_latest_profile_client'
