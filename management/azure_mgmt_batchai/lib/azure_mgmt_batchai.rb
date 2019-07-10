0d71fc9cdd350254606b6674f05ef844b0a53be1
MyronFanQiu <fanqiu@microsoft.com>
2019-07-10 17:19:08 +0800

    Regen SDK for BatchAI

---
+++ management/azure_mgmt_batchai/lib/azure_mgmt_batchai.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '2018-03-01/generated/azure_mgmt_batchai'
+require '2017-09-01-preview/generated/azure_mgmt_batchai'
+require '2018-05-01/generated/azure_mgmt_batchai'
+require 'profiles/latest/batchai_latest_profile_client'
