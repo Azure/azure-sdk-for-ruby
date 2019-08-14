87c7b8fcb2263802c647b7c6f3420a49430b5760
MyronFanQiu <fanqiu@microsoft.com>
2019-08-14 13:13:29 +0800

    fix for version

--- management/azure_mgmt_cosmosdb/lib/2015-04-08/generated/azure_mgmt_cosmosdb/cosmos_db.rb
+++ management/azure_mgmt_cosmosdb/lib/2015-04-08/generated/azure_mgmt_cosmosdb/cosmos_db.rb
@@ -173,7 +173,7 @@ module Azure::Cosmosdb::Mgmt::V2015_04_08
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_cosmosdb'
-        sdk_information = "#{sdk_information}/0.18.0"
+        sdk_information = "#{sdk_information}/0.17.0"
         add_user_agent_information(sdk_information)
     end
   end
