630bd9d07f6af7d5f1a1c505686a222d7605a6cf
Bin Ma <bim@microsoft.com>
2019-11-12 10:37:16 +0800

    Regen azure_mgmt_cosmosdb 2019-08-01-preview

--- management/azure_mgmt_cosmosdb/lib/azure_mgmt_cosmosdb.rb
+++ management/azure_mgmt_cosmosdb/lib/azure_mgmt_cosmosdb.rb
@@ -4,4 +4,5 @@
 
 require '2015-04-08/generated/azure_mgmt_cosmosdb'
 require '2019-08-01/generated/azure_mgmt_cosmosdb'
+require '2019-08-01-preview/generated/azure_mgmt_cosmosdb'
 require 'profiles/latest/cosmosdb_latest_profile_client'
