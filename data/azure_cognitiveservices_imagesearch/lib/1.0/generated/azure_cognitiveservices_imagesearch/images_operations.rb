c2a357deac1d93be2254105ae701735c8f642025
Bin Ma <bim@microsoft.com>
2019-11-19 15:22:29 +0800

    regen imagesearch

--- data/azure_cognitiveservices_imagesearch/lib/1.0/generated/azure_cognitiveservices_imagesearch/images_operations.rb
+++ data/azure_cognitiveservices_imagesearch/lib/1.0/generated/azure_cognitiveservices_imagesearch/images_operations.rb
@@ -1002,6 +1002,8 @@ module Azure::CognitiveServices::ImageSearch::V1_0
         end
 
         result.request_id = http_response['x-ms-request-id'] unless http_response['x-ms-request-id'].nil?
+        result.correlation_request_id = http_response['x-ms-correlation-request-id'] unless http_response['x-ms-correlation-request-id'].nil?
+        result.client_request_id = http_response['x-ms-client-request-id'] unless http_response['x-ms-client-request-id'].nil?
         # Deserialize Response
         if status_code == 200
           begin
@@ -1909,6 +1911,8 @@ module Azure::CognitiveServices::ImageSearch::V1_0
         end
 
         result.request_id = http_response['x-ms-request-id'] unless http_response['x-ms-request-id'].nil?
+        result.correlation_request_id = http_response['x-ms-correlation-request-id'] unless http_response['x-ms-correlation-request-id'].nil?
+        result.client_request_id = http_response['x-ms-client-request-id'] unless http_response['x-ms-client-request-id'].nil?
         # Deserialize Response
         if status_code == 200
           begin
@@ -2511,6 +2515,8 @@ module Azure::CognitiveServices::ImageSearch::V1_0
         end
 
         result.request_id = http_response['x-ms-request-id'] unless http_response['x-ms-request-id'].nil?
+        result.correlation_request_id = http_response['x-ms-correlation-request-id'] unless http_response['x-ms-correlation-request-id'].nil?
+        result.client_request_id = http_response['x-ms-client-request-id'] unless http_response['x-ms-client-request-id'].nil?
         # Deserialize Response
         if status_code == 200
           begin
