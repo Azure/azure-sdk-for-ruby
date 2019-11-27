03609d54d14723d9219be56537e3622e8d2c9a5e
Bin Ma <bim@microsoft.com>
2019-11-27 10:09:02 +0800

    azure_mgmt_portal 0.17.0

---
+++ management/azure_mgmt_portal/lib/azure_mgmt_portal.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
+require '2015-08-01-preview/generated/azure_mgmt_portal'
+require '2018-10-01-preview/generated/azure_mgmt_portal'
+require '2019-01-01-preview/generated/azure_mgmt_portal'
