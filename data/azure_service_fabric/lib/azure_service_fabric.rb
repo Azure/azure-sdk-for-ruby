0d050af0c3c692adfff79d1955e50abadd79946a
Bin Ma <bim@microsoft.com>
2019-12-04 12:42:28 +0800

    azure_service_fabric 0.17.2 -> 0.18.0

--- data/azure_service_fabric/lib/azure_service_fabric.rb
+++ data/azure_service_fabric/lib/azure_service_fabric.rb
@@ -2,8 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require '6.4.0.36/generated/azure_service_fabric'
-require '6.5.0.36/generated/azure_service_fabric'
 require '6.2.0.9/generated/azure_service_fabric'
 require '6.3.0.9/generated/azure_service_fabric'
+require '6.4.0.36/generated/azure_service_fabric'
+require '6.5.0.36/generated/azure_service_fabric'
+require '7.0.0.42/generated/azure_service_fabric'
 require 'profiles/latest/servicefabric_latest_profile_client'
