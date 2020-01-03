71fd44fd6adf34bbd90314e283bc49e3d6a24b37
Bin Ma <bim@microsoft.com>
2020-01-03 15:31:05 +0800

    Release 20200103 (#2743)

--- azure_sdk/lib/azure_sdk/version.rb
+++ azure_sdk/lib/azure_sdk/version.rb
@@ -3,5 +3,5 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
 module Azure
-  VERSION = '0.49.0'
+  VERSION = '0.49.1'
 end
--- management/azure_mgmt_netapp/lib/2019-07-01/generated/azure_mgmt_netapp/azure_net_app_files_management_client.rb
+++ management/azure_mgmt_netapp/lib/2019-07-01/generated/azure_mgmt_netapp/azure_net_app_files_management_client.rb
@@ -412,7 +412,7 @@ module Azure::NetApp::Mgmt::V2019_07_01
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_netapp'
-        sdk_information = "#{sdk_information}/0.18.2"
+        sdk_information = "#{sdk_information}/0.18.3"
         add_user_agent_information(sdk_information)
     end
   end
