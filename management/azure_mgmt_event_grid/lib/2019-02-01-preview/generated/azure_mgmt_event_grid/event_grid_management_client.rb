f3cb8778e7c62a183a253c113e4464a36cc53d40
MyronFanQiu <fanqiu@microsoft.com>
2020-03-20 12:28:11 +0800

    bump version for event grid

--- management/azure_mgmt_event_grid/lib/2019-02-01-preview/generated/azure_mgmt_event_grid/event_grid_management_client.rb
+++ management/azure_mgmt_event_grid/lib/2019-02-01-preview/generated/azure_mgmt_event_grid/event_grid_management_client.rb
@@ -146,7 +146,7 @@ module Azure::EventGrid::Mgmt::V2019_02_01_preview
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_event_grid'
-        sdk_information = "#{sdk_information}/0.17.11"
+        sdk_information = "#{sdk_information}/0.18.0"
         add_user_agent_information(sdk_information)
     end
   end
