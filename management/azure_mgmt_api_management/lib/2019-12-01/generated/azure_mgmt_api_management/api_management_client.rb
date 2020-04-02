aa5ade5b5432266cb74ff4c3e06497c272e1d4ed
MyronFanQiu <fanqiu@microsoft.com>
2020-04-02 11:20:23 +0800

    bump version and regen

--- management/azure_mgmt_api_management/lib/2019-12-01/generated/azure_mgmt_api_management/api_management_client.rb
+++ management/azure_mgmt_api_management/lib/2019-12-01/generated/azure_mgmt_api_management/api_management_client.rb
@@ -378,7 +378,7 @@ module Azure::ApiManagement::Mgmt::V2019_12_01
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_api_management'
-        sdk_information = "#{sdk_information}/0.18.5"
+        sdk_information = "#{sdk_information}/0.19.0"
         add_user_agent_information(sdk_information)
     end
   end
