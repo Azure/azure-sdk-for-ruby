0222fe37d176b469e632340dfd212662db896912
MyronFanQiu <fanqiu@microsoft.com>
2020-05-14 16:40:29 +0800

    regen azure_mgmt_network

--- management/azure_mgmt_network/lib/2019-06-01/generated/azure_mgmt_network/network_management_client.rb
+++ management/azure_mgmt_network/lib/2019-06-01/generated/azure_mgmt_network/network_management_client.rb
@@ -664,7 +664,7 @@ module Azure::Network::Mgmt::V2019_06_01
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_network'
-        sdk_information = "#{sdk_information}/0.23.2"
+        sdk_information = "#{sdk_information}/0.23.3"
         add_user_agent_information(sdk_information)
     end
   end
