132c2636ce38d20edcddb77e9553f87dedf80afb
MyronFanQiu <fanqiu@microsoft.com>
2020-06-16 13:23:32 +0800

    regen sdk

--- management/azure_mgmt_security/lib/2019-01-01/generated/azure_mgmt_security/security_center.rb
+++ management/azure_mgmt_security/lib/2019-01-01/generated/azure_mgmt_security/security_center.rb
@@ -132,7 +132,7 @@ module Azure::Security::Mgmt::V2019_01_01
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_security'
-        sdk_information = "#{sdk_information}/0.18.1"
+        sdk_information = "#{sdk_information}/0.18.2"
         add_user_agent_information(sdk_information)
     end
   end
--- management/azure_mgmt_security/lib/azure_mgmt_security.rb
+++ management/azure_mgmt_security/lib/azure_mgmt_security.rb
@@ -3,10 +3,12 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
 require '2018-06-01/generated/azure_mgmt_security'
+require '2020-01-01-preview/generated/azure_mgmt_security'
 require '2017-08-01-preview/generated/azure_mgmt_security'
 require '2019-01-01-preview/generated/azure_mgmt_security'
 require '2017-08-01/generated/azure_mgmt_security'
 require '2015-06-01-preview/generated/azure_mgmt_security'
 require '2019-01-01/generated/azure_mgmt_security'
+require '2020-01-01/generated/azure_mgmt_security'
 require '2019-08-01/generated/azure_mgmt_security'
 require 'profiles/latest/security_latest_profile_client'
