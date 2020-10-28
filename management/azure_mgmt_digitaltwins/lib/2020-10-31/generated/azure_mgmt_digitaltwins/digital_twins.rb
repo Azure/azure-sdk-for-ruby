777409c591601613e3c62862014b4e7b6a2d336d
Kai Ru <kairu@microsoft.com>
2020-10-28 16:37:45 +0800

    regen azure_mgmt_digitaltwins:v0.18.0

--- management/azure_mgmt_digitaltwins/lib/2020-10-31/generated/azure_mgmt_digitaltwins/digital_twins.rb
+++ management/azure_mgmt_digitaltwins/lib/2020-10-31/generated/azure_mgmt_digitaltwins/digital_twins.rb
@@ -194,6 +194,8 @@ module Azure::DigitalTwins::Mgmt::V2020_10_31
     end
 
     #
+    # Update metadata of DigitalTwinsInstance.
+    #
     # @param resource_group_name [String] The name of the resource group that
     # contains the DigitalTwinsInstance.
     # @param resource_name [String] The name of the DigitalTwinsInstance.
@@ -202,25 +204,92 @@ module Azure::DigitalTwins::Mgmt::V2020_10_31
     # @param custom_headers [Hash{String => String}] A hash of custom headers that
     # will be added to the HTTP request.
     #
-    # @return [Concurrent::Promise] promise which provides async access to http
-    # response.
+    # @return [MsRestAzure::AzureOperationResponse] HTTP response information.
+    #
+    def update_with_http_info(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:nil)
+      update_async(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:custom_headers).value!
+    end
+
+    #
+    # Update metadata of DigitalTwinsInstance.
+    #
+    # @param resource_group_name [String] The name of the resource group that
+    # contains the DigitalTwinsInstance.
+    # @param resource_name [String] The name of the DigitalTwinsInstance.
+    # @param digital_twins_patch_description [DigitalTwinsPatchDescription] The
+    # DigitalTwinsInstance and security metadata.
+    # @param [Hash{String => String}] A hash of custom headers that will be added
+    # to the HTTP request.
+    #
+    # @return [Concurrent::Promise] Promise object which holds the HTTP response.
     #
     def update_async(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:nil)
-      # Send request
-      promise = begin_update_async(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:custom_headers)
+      fail ArgumentError, "'@client.api_version' should satisfy the constraint - 'MinLength': '10'" if !@client.api_version.nil? && @client.api_version.length < 10
+      @client.api_version = '2020-10-31'
+      fail ArgumentError, '@client.subscription_id is nil' if @client.subscription_id.nil?
+      fail ArgumentError, 'resource_group_name is nil' if resource_group_name.nil?
+      fail ArgumentError, "'resource_group_name' should satisfy the constraint - 'MaxLength': '64'" if !resource_group_name.nil? && resource_group_name.length > 64
+      fail ArgumentError, "'resource_group_name' should satisfy the constraint - 'MinLength': '1'" if !resource_group_name.nil? && resource_group_name.length < 1
+      fail ArgumentError, 'resource_name is nil' if resource_name.nil?
+      fail ArgumentError, "'resource_name' should satisfy the constraint - 'MaxLength': '63'" if !resource_name.nil? && resource_name.length > 63
+      fail ArgumentError, "'resource_name' should satisfy the constraint - 'MinLength': '3'" if !resource_name.nil? && resource_name.length < 3
+      fail ArgumentError, "'resource_name' should satisfy the constraint - 'Pattern': '^(?!-)[A-Za-z0-9-]{3,63}(?<!-)$'" if !resource_name.nil? && resource_name.match(Regexp.new('^^(?!-)[A-Za-z0-9-]{3,63}(?<!-)$$')).nil?
+      fail ArgumentError, 'digital_twins_patch_description is nil' if digital_twins_patch_description.nil?
 
-      promise = promise.then do |response|
-        # Defining deserialization method.
-        deserialize_method = lambda do |parsed_response|
-          result_mapper = Azure::DigitalTwins::Mgmt::V2020_10_31::Models::DigitalTwinsDescription.mapper()
-          parsed_response = @client.deserialize(result_mapper, parsed_response)
+
+      request_headers = {}
+      request_headers['Content-Type'] = 'application/json; charset=utf-8'
+
+      # Set Headers
+      request_headers['x-ms-client-request-id'] = SecureRandom.uuid
+      request_headers['accept-language'] = @client.accept_language unless @client.accept_language.nil?
+
+      # Serialize Request
+      request_mapper = Azure::DigitalTwins::Mgmt::V2020_10_31::Models::DigitalTwinsPatchDescription.mapper()
+      request_content = @client.serialize(request_mapper,  digital_twins_patch_description)
+      request_content = request_content != nil ? JSON.generate(request_content, quirks_mode: true) : nil
+
+      path_template = 'subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DigitalTwins/digitalTwinsInstances/{resourceName}'
+
+      request_url = @base_url || @client.base_url
+
+      options = {
+          middlewares: [[MsRest::RetryPolicyMiddleware, times: 3, retry: 0.02], [:cookie_jar]],
+          path_params: {'subscriptionId' => @client.subscription_id,'resourceGroupName' => resource_group_name,'resourceName' => resource_name},
+          query_params: {'api-version' => @client.api_version},
+          body: request_content,
+          headers: request_headers.merge(custom_headers || {}),
+          base_url: request_url
+      }
+      promise = @client.make_request_async(:patch, path_template, options)
+
+      promise = promise.then do |result|
+        http_response = result.response
+        status_code = http_response.status
+        response_content = http_response.body
+        unless status_code == 200
+          error_model = JSON.load(response_content)
+          fail MsRest::HttpOperationError.new(result.request, http_response, error_model)
         end
 
-        # Waiting for response.
-        @client.get_long_running_operation_result(response, deserialize_method)
+        result.request_id = http_response['x-ms-request-id'] unless http_response['x-ms-request-id'].nil?
+        result.correlation_request_id = http_response['x-ms-correlation-request-id'] unless http_response['x-ms-correlation-request-id'].nil?
+        result.client_request_id = http_response['x-ms-client-request-id'] unless http_response['x-ms-client-request-id'].nil?
+        # Deserialize Response
+        if status_code == 200
+          begin
+            parsed_response = response_content.to_s.empty? ? nil : JSON.load(response_content)
+            result_mapper = Azure::DigitalTwins::Mgmt::V2020_10_31::Models::DigitalTwinsDescription.mapper()
+            result.body = @client.deserialize(result_mapper, parsed_response)
+          rescue Exception => e
+            fail MsRest::DeserializationError.new('Error occurred in deserializing the response', e.message, e.backtrace, result)
+          end
+        end
+
+        result
       end
 
-      promise
+      promise.execute
     end
 
     #
@@ -680,123 +749,6 @@ module Azure::DigitalTwins::Mgmt::V2020_10_31
         end
         # Deserialize Response
         if status_code == 201
-          begin
-            parsed_response = response_content.to_s.empty? ? nil : JSON.load(response_content)
-            result_mapper = Azure::DigitalTwins::Mgmt::V2020_10_31::Models::DigitalTwinsDescription.mapper()
-            result.body = @client.deserialize(result_mapper, parsed_response)
-          rescue Exception => e
-            fail MsRest::DeserializationError.new('Error occurred in deserializing the response', e.message, e.backtrace, result)
-          end
-        end
-
-        result
-      end
-
-      promise.execute
-    end
-
-    #
-    # Update metadata of DigitalTwinsInstance.
-    #
-    # @param resource_group_name [String] The name of the resource group that
-    # contains the DigitalTwinsInstance.
-    # @param resource_name [String] The name of the DigitalTwinsInstance.
-    # @param digital_twins_patch_description [DigitalTwinsPatchDescription] The
-    # DigitalTwinsInstance and security metadata.
-    # @param custom_headers [Hash{String => String}] A hash of custom headers that
-    # will be added to the HTTP request.
-    #
-    # @return [DigitalTwinsDescription] operation results.
-    #
-    def begin_update(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:nil)
-      response = begin_update_async(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:custom_headers).value!
-      response.body unless response.nil?
-    end
-
-    #
-    # Update metadata of DigitalTwinsInstance.
-    #
-    # @param resource_group_name [String] The name of the resource group that
-    # contains the DigitalTwinsInstance.
-    # @param resource_name [String] The name of the DigitalTwinsInstance.
-    # @param digital_twins_patch_description [DigitalTwinsPatchDescription] The
-    # DigitalTwinsInstance and security metadata.
-    # @param custom_headers [Hash{String => String}] A hash of custom headers that
-    # will be added to the HTTP request.
-    #
-    # @return [MsRestAzure::AzureOperationResponse] HTTP response information.
-    #
-    def begin_update_with_http_info(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:nil)
-      begin_update_async(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:custom_headers).value!
-    end
-
-    #
-    # Update metadata of DigitalTwinsInstance.
-    #
-    # @param resource_group_name [String] The name of the resource group that
-    # contains the DigitalTwinsInstance.
-    # @param resource_name [String] The name of the DigitalTwinsInstance.
-    # @param digital_twins_patch_description [DigitalTwinsPatchDescription] The
-    # DigitalTwinsInstance and security metadata.
-    # @param [Hash{String => String}] A hash of custom headers that will be added
-    # to the HTTP request.
-    #
-    # @return [Concurrent::Promise] Promise object which holds the HTTP response.
-    #
-    def begin_update_async(resource_group_name, resource_name, digital_twins_patch_description, custom_headers:nil)
-      fail ArgumentError, "'@client.api_version' should satisfy the constraint - 'MinLength': '10'" if !@client.api_version.nil? && @client.api_version.length < 10
-      @client.api_version = '2020-10-31'
-      fail ArgumentError, '@client.subscription_id is nil' if @client.subscription_id.nil?
-      fail ArgumentError, 'resource_group_name is nil' if resource_group_name.nil?
-      fail ArgumentError, "'resource_group_name' should satisfy the constraint - 'MaxLength': '64'" if !resource_group_name.nil? && resource_group_name.length > 64
-      fail ArgumentError, "'resource_group_name' should satisfy the constraint - 'MinLength': '1'" if !resource_group_name.nil? && resource_group_name.length < 1
-      fail ArgumentError, 'resource_name is nil' if resource_name.nil?
-      fail ArgumentError, "'resource_name' should satisfy the constraint - 'MaxLength': '63'" if !resource_name.nil? && resource_name.length > 63
-      fail ArgumentError, "'resource_name' should satisfy the constraint - 'MinLength': '3'" if !resource_name.nil? && resource_name.length < 3
-      fail ArgumentError, "'resource_name' should satisfy the constraint - 'Pattern': '^(?!-)[A-Za-z0-9-]{3,63}(?<!-)$'" if !resource_name.nil? && resource_name.match(Regexp.new('^^(?!-)[A-Za-z0-9-]{3,63}(?<!-)$$')).nil?
-      fail ArgumentError, 'digital_twins_patch_description is nil' if digital_twins_patch_description.nil?
-
-
-      request_headers = {}
-      request_headers['Content-Type'] = 'application/json; charset=utf-8'
-
-      # Set Headers
-      request_headers['x-ms-client-request-id'] = SecureRandom.uuid
-      request_headers['accept-language'] = @client.accept_language unless @client.accept_language.nil?
-
-      # Serialize Request
-      request_mapper = Azure::DigitalTwins::Mgmt::V2020_10_31::Models::DigitalTwinsPatchDescription.mapper()
-      request_content = @client.serialize(request_mapper,  digital_twins_patch_description)
-      request_content = request_content != nil ? JSON.generate(request_content, quirks_mode: true) : nil
-
-      path_template = 'subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DigitalTwins/digitalTwinsInstances/{resourceName}'
-
-      request_url = @base_url || @client.base_url
-
-      options = {
-          middlewares: [[MsRest::RetryPolicyMiddleware, times: 3, retry: 0.02], [:cookie_jar]],
-          path_params: {'subscriptionId' => @client.subscription_id,'resourceGroupName' => resource_group_name,'resourceName' => resource_name},
-          query_params: {'api-version' => @client.api_version},
-          body: request_content,
-          headers: request_headers.merge(custom_headers || {}),
-          base_url: request_url
-      }
-      promise = @client.make_request_async(:patch, path_template, options)
-
-      promise = promise.then do |result|
-        http_response = result.response
-        status_code = http_response.status
-        response_content = http_response.body
-        unless status_code == 200
-          error_model = JSON.load(response_content)
-          fail MsRest::HttpOperationError.new(result.request, http_response, error_model)
-        end
-
-        result.request_id = http_response['x-ms-request-id'] unless http_response['x-ms-request-id'].nil?
-        result.correlation_request_id = http_response['x-ms-correlation-request-id'] unless http_response['x-ms-correlation-request-id'].nil?
-        result.client_request_id = http_response['x-ms-client-request-id'] unless http_response['x-ms-client-request-id'].nil?
-        # Deserialize Response
-        if status_code == 200
           begin
             parsed_response = response_content.to_s.empty? ? nil : JSON.load(response_content)
             result_mapper = Azure::DigitalTwins::Mgmt::V2020_10_31::Models::DigitalTwinsDescription.mapper()
