79eab91db43649bb1f94f7501f42bb1f7bb11577
Kai Ru <kairu@microsoft.com>
2020-10-28 16:14:34 +0800

    regen azure_mgmt_kusto:V0.20.0

--- management/azure_mgmt_kusto/lib/2020-06-14/generated/azure_mgmt_kusto/kusto_management_client.rb
+++ management/azure_mgmt_kusto/lib/2020-06-14/generated/azure_mgmt_kusto/kusto_management_client.rb
@@ -150,7 +150,7 @@ module Azure::Kusto::Mgmt::V2020_06_14
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_kusto'
-        sdk_information = "#{sdk_information}/0.19.3"
+        sdk_information = "#{sdk_information}/0.20.0"
         add_user_agent_information(sdk_information)
     end
   end
