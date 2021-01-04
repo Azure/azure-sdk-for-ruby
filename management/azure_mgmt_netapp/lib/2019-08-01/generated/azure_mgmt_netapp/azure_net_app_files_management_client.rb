7bea6820cf6e6a0d9c04c0a75dabf20a1df8f02e
Kai Ru <kairu@microsoft.com>
2021-01-04 11:56:58 +0800

    generate azure_mgmt_netapp:v0.22.0

--- management/azure_mgmt_netapp/lib/2019-08-01/generated/azure_mgmt_netapp/azure_net_app_files_management_client.rb
+++ management/azure_mgmt_netapp/lib/2019-08-01/generated/azure_mgmt_netapp/azure_net_app_files_management_client.rb
@@ -150,7 +150,7 @@ module Azure::NetApp::Mgmt::V2019_08_01
     #
     def add_telemetry
         sdk_information = 'azure_mgmt_netapp'
-        sdk_information = "#{sdk_information}/0.21.0"
+        sdk_information = "#{sdk_information}/0.22.0"
         add_user_agent_information(sdk_information)
     end
   end
