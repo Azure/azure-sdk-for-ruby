9cdcd7512238c4cef96d42611e8ae208567b2107
Kai Ru <kairu@microsoft.com>
2021-02-03 11:20:09 +0800

    regenereate azure_mgmt_cosmosdb:0.23.1

--- management/azure_mgmt_cosmosdb/lib/profiles/latest/cosmosdb_module_definition.rb
+++ management/azure_mgmt_cosmosdb/lib/profiles/latest/cosmosdb_module_definition.rb
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-module Azure::Cosmosdb::Mgmt
-    VERSION = '0.23.1'
-end
+module Azure end
+module Azure::Cosmosdb end
+module Azure::Cosmosdb::Profiles end
+module Azure::Cosmosdb::Profiles::Latest end
